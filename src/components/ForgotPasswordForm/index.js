0a36fe09dfa3849f8356a7ae6b4ae33066287a22
Jesus Perez <jesusdpch00@gmail.com>
2020-09-16 15:18:32 -0500

    [FEAT] handling errors

--- src/components/ForgotPasswordForm/index.js
+++ src/components/ForgotPasswordForm/index.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { Alert } from '../Confirm'
 import {
   ForgotPasswordForm as ForgotPasswordController,
   useLanguage
 } from 'ordering-components'
 import {
-  LoginContainer,
+  ForgotPasswordContainer,
   FormSide,
   HeroSide,
   FormInput,
@@ -20,44 +22,101 @@ import { Button } from '../../styles/Buttons'
 const ForgotPasswordUI = (props) => {
   const {
     hanldeChangeInput,
-    handleButtonForgotPasswordClick
+    handleButtonForgotPasswordClick,
+    formState,
+    formData
   } = props
+
+  const { handleSubmit, register, errors } = useForm()
+  const [alertState, setAlertState] = useState({ open: false, title: '', content: [], success: false })
   const [, t] = useLanguage()
+
+  useEffect(() => {
+    if (Object.keys(errors).length > 0) {
+      setAlertState({
+        ...alertState,
+        success: false,
+        open: true,
+        title: t('ERROR_FORGOT_ERROR'),
+        content: Object.values(errors).map(error => error.message)
+      })
+    }
+  }, [errors])
+
+  useEffect(() => {
+    if (!formState.loading && formState.result?.error) {
+      setAlertState({
+        ...alertState,
+        success: false,
+        open: true,
+        title: t('ERROR_FORGOT_ERROR'),
+        content: formState.result?.result || [t('ERROR')]
+      })
+    }
+    if (!formState.loading && !formState.result?.error && alertState.success) {
+      setAlertState({
+        ...alertState,
+        open: true,
+        title: t('LINK_SEND_SUCCESSFULLY'),
+        content: t('SUCCESS_SEND_FORGOT_PASSWORD').replace('_email_', formData.email)
+      })
+    }
+  }, [formState.loading])
+
+  const onSubmit = () => {
+    setAlertState({ ...alertState, success: true })
+    handleButtonForgotPasswordClick()
+  }
+
+  const closeAlert = () => {
+    setAlertState({
+      ...alertState,
+      open: false,
+      content: []
+    })
+  }
   return (
-    <LoginContainer>
+    <ForgotPasswordContainer>
       <HeroSide>
         <TitleHeroSide>
-          <h1>Forgot your password?</h1>
-          <p>Enter your email addres and we'll send you a link to reset your password.</p>
+          <h1>{t('TITLE_FORGOT_PASSWORD', 'Forgot your password?')}</h1>
+          <p>{t('SUBTITLE_FORGOT_PASSWORD', "Enter your email addres and we'll send you a link to reset your password.")}</p>
         </TitleHeroSide>
-        {/* <div style={{ position: "absolute" }}>
-          <img
-            src={triangle}
-            style={{
-              display: "inline-block",
-              width: "1000px",
-              height: "700px",
-            }}
-          />
-        </div> */}
       </HeroSide>
       <FormSide>
-        <img src={logoHeader} alt='Logo login' />
+        <img src={logoHeader} alt='Logo' />
         {(
-          <FormInput>
+          <FormInput onSubmit={handleSubmit(onSubmit)} noValidate>
             <Input
-              type='email'
+              type='text'
               name='email'
+              spellcheck='false'
               placeholder={t('EMAIL')}
               onChange={(e) => hanldeChangeInput(e)}
+              ref={register({
+                required: t('VALIDATION_ERROR_REQUIRED', 'Email is required').replace('_attribute_', t('EMAIL', 'Email')),
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: t('VALIDATION_ERROR_EMAIL', 'Invalid email address').replace('_attribute_', t('EMAIL', 'Email'))
+                }
+              })}
             />
-            <Button color='primary' onClick={() => handleButtonForgotPasswordClick()}>
-              {t('FRONT_RECOVER_PASSWORD')}
+            <Button color={formState.loading || alertState.success ? 'secondary' : 'primary'} type='submit' disabled={formState.loading || alertState.success}>
+              {alertState.success && formState.result.result ? t('LINK_SEND_FORGOT_PASSWORD') : t('FRONT_RECOVER_PASSWORD')}
             </Button>
           </FormInput>
         )}
       </FormSide>
-    </LoginContainer>
+      <Alert
+        title={alertState.title}
+        content={alertState.content}
+        acceptText={t('ACCEPT')}
+        open={alertState.open}
+        onClose={() => closeAlert()}
+        onAccept={() => closeAlert()}
+        closeOnBackdrop={false}
+      />
+    </ForgotPasswordContainer>
   )
 }
 
