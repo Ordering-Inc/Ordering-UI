9e165b39643d80dbfedf8616fb482eff35458077
Jesus Perez <jesusdpch00@gmail.com>
2020-09-30 21:09:43 -0500

    [FIX] issues text and z-index of categories

--- src/components/BusinessProductsCategories/styles.js
+++ src/components/BusinessProductsCategories/styles.js
@@ -5,8 +5,9 @@ export const CategoriesContainer = styled.div`
   overflow-x: auto;
   padding: 15px;
   background: #F8F8F8;
-  z-index: 1000;
+  /*z-index: 1000;*/
   position: sticky;
   top: 0;
   border-bottom: 1px solid #D9D9D9;
+  text-align: center;
 `
