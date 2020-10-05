5e3c49bc60a37bf79f383ae6f8007f6f5a00f7c4
carlos quiroz <carloseqv29596@gmail.com>
2020-10-05 18:07:17 -0400

    [FEAT] close address popover when select one after registration

--- src/components/AddressesPopover/index.js
+++ src/components/AddressesPopover/index.js
@@ -66,6 +66,7 @@ export const AddressesPopover = (props) => {
               <AddressList
                 popover
                 changeOrderAddressWithDefault
+                onClosePopover={props.onClose}
               />
             </>
           )
