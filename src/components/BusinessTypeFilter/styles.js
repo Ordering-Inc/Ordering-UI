02cee07822e21ff56a9f13e7b507a227b9efd9c1
Jesus Perez <jesusdpch00@gmail.com>
2020-10-05 23:23:56 -0400

    [ADD] new design for businessCategories

--- src/components/BusinessTypeFilter/styles.js
+++ src/components/BusinessTypeFilter/styles.js
@@ -1,13 +1,31 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const TypeContainer = styled.div`
   display: flex;
   width: 100%;
   overflow-x: auto;
-
+  justify-content: center;
   div.category {
     text-overflow: ellipsis;
     white-space: nowrap;
     overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
   }
+
+`
+
+export const ImageContainer = styled.div`
+    img{
+      border-radius: 50%;
+      width: 150px;
+    }
+     ${({ active }) => active && css`
+      img{
+        border: 1px solid red ;
+      }
+  `}
 `
