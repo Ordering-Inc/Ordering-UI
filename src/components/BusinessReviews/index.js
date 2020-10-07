947694607767621af14a5ec0bb9c4d954bf31917
Jesus Perez <jesusdpch00@gmail.com>
2020-10-07 14:51:27 -0400

    [ADD] handling errors for businessReviews

--- src/components/BusinessReviews/index.js
+++ src/components/BusinessReviews/index.js
@@ -28,51 +28,53 @@ export const BusinessReviewsUI = (props) => {
   })
   return (
     <>
-      <>
-        <ReviewOf>
-          {!reviewsList.loading ? <h3>Reviews of {businessName}</h3> : <Skeleton width={200} />}
-          {!reviewsList.loading ? <Select options={options} defaultValue={options[options.length - 1]?.showOnSelected} onChange={(val) => handleClickOption(val)} notAsync InitialIcon={AiOutlineStar} /> : <Skeleton width={200} height={30} />}
-        </ReviewOf>
-        <Content>
-          <h3>{!reviewsList.loading ? <><AiOutlineStar color='#D81212' />{stars}</> : <Skeleton width={100} height={30} />}</h3>
-          {!reviewsList.loading ? reviewsList?.reviews.map((review) => (
-            <ReviewContainer key={review.id}>
-              <Comments>
-                <div>
-                  <p><AiOutlineStar color='#D81212' /> {review.total}</p>
-                </div>
-                <Comment>
-                  <p title={review.comment}>{review.comment}</p>
-                </Comment>
-              </Comments>
-              <Scores>
-                <Score star={review.quality} text={t('QUALITY_OF_PRODUCTS', 'Quality of products')} />
-                <Score star={review.delivery} text={t('PUNCTUALITY', 'Punctuality')} />
-                <Score star={review.service} text={t('SERVICE', 'Service')} />
-                <Score star={review.package} text={t('PRODUCT_PACKAGING', 'Product Packaging')} />
-              </Scores>
-            </ReviewContainer>
-          )) : (
-            <>
-              {[...Array(2)].map((item, i) => (
-                <SkeletonContainer key={i}>
+      {reviewsList.error ? <h2>{t('ERROR_UNKNOWN', 'An error has ocurred')}</h2> : (
+        <>
+          <ReviewOf>
+            {!reviewsList.loading ? <h3>Reviews of {businessName}</h3> : <Skeleton width={200} />}
+            {!reviewsList.loading ? <Select options={options} defaultValue={options[options.length - 1]?.showOnSelected} onChange={(val) => handleClickOption(val)} notAsync InitialIcon={AiOutlineStar} /> : <Skeleton width={200} height={30} />}
+          </ReviewOf>
+          <Content>
+            <h3>{!reviewsList.loading ? <><AiOutlineStar color='#D81212' />{stars}</> : <Skeleton width={100} height={30} />}</h3>
+            {!reviewsList.loading ? reviewsList?.reviews.map((review) => (
+              <ReviewContainer key={review.id}>
+                <Comments>
                   <div>
-                    <Skeleton width={100} height={30} />
-                    <Skeleton width={100} />
-                    <Skeleton width={100} />
+                    <p><AiOutlineStar color='#D81212' /> {review.total}</p>
                   </div>
-                  <div>
-                    <Skeleton width={150} height={50} />
-                    <Skeleton width={150} height={50} />
-                    <Skeleton width={150} height={50} />
-                    <Skeleton width={150} height={50} />
-                  </div>
-                </SkeletonContainer>
-              ))}
-            </>
-          )}
-        </Content>
-      </>
+                  <Comment>
+                    <p title={review.comment}>{review.comment}</p>
+                  </Comment>
+                </Comments>
+                <Scores>
+                  <Score star={review.quality} text={t('QUALITY_OF_PRODUCTS', 'Quality of products')} />
+                  <Score star={review.delivery} text={t('PUNCTUALITY', 'Punctuality')} />
+                  <Score star={review.service} text={t('SERVICE', 'Service')} />
+                  <Score star={review.package} text={t('PRODUCT_PACKAGING', 'Product Packaging')} />
+                </Scores>
+              </ReviewContainer>
+            )) : (
+              <>
+                {[...Array(2)].map((item, i) => (
+                  <SkeletonContainer key={i}>
+                    <div>
+                      <Skeleton width={100} height={30} />
+                      <Skeleton width={100} />
+                      <Skeleton width={100} />
+                    </div>
+                    <div>
+                      <Skeleton width={150} height={50} />
+                      <Skeleton width={150} height={50} />
+                      <Skeleton width={150} height={50} />
+                      <Skeleton width={150} height={50} />
+                    </div>
+                  </SkeletonContainer>
+                ))}
+              </>
+            )}
+          </Content>
+        </>
+      )}
     </>
   )
 }
