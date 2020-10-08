fe3de53cb130cda1226b636c9de58941f16a49c7
carlos quiroz <carloseqv29596@gmail.com>
2020-10-08 19:39:53 -0400

    [ADD] some changes

--- template/Pages/BusinessesList/index.js
+++ template/Pages/BusinessesList/index.js
@@ -3,14 +3,19 @@ import { BusinessesListing } from '../../../src/components/BusinessesListing'
 import { useHistory } from 'react-router-dom'
 
 export const BusinessesList = (props) => {
-  const propsToFetch = ['id', 'name', 'header', 'logo', 'name', 'schedule', 'open', 'delivery_price', 'distance', 'delivery_time', 'pickup_time', 'reviews', 'featured', 'offers', 'food', 'laundry', 'alcohol', 'groceries', 'slug']
   const history = useHistory()
 
-  const handleBusinessClick = (business) => {
-    history.push(`/store/${business.slug}`)
+  const businessListingProps = {
+    ...props,
+    isSearchByName: true,
+    isSearchByDescription: true,
+    onBusinessClick: (business) => {
+      history.push(`/store/${business.slug}`)
+    },
+    propsToFetch: ['id', 'name', 'header', 'logo', 'name', 'schedule', 'open', 'delivery_price', 'distance', 'delivery_time', 'pickup_time', 'reviews', 'featured', 'offers', 'food', 'laundry', 'alcohol', 'groceries', 'slug']
   }
 
   return (
-    <BusinessesListing {...props} propsToFetch={propsToFetch} onBusinessClick={handleBusinessClick} />
+    <BusinessesListing {...businessListingProps} />
   )
 }
