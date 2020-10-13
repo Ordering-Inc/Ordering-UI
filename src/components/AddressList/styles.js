05774fdc91190c3fc0fdd1afe0f0d70285427c49
carlos quiroz <carloseqv29596@gmail.com>
2020-10-13 18:49:48 -0400

    [FIX] addresslist modal

--- src/components/AddressList/styles.js
+++ src/components/AddressList/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const AddressListContainer = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 10px;
+  padding: 0px 10px;
   max-height: 60vh;
   overflow: auto;
 
