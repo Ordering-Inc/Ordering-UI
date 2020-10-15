ef8dfd4d14baa9a1f068e2ceecb32562500e186a
carlos quiroz <carloseqv29596@gmail.com>
2020-10-14 22:10:12 -0400

    [FIX] typo and input placeholder color

--- src/components/BusinessesListing/index.js
+++ src/components/BusinessesListing/index.js
@@ -81,7 +81,7 @@ const BusinessesListingUI = (props) => {
         <SearchBar
           onSearch={handleChangeSearch}
           search={searchValue}
-          placeholder={t('SEARCH_BUSINESS', 'Search Business')}
+          placeholder={t('SEARCH_BUSINESS', 'Search Businesses')}
         />
       </WrapperSearch>
       <BusinessList>
