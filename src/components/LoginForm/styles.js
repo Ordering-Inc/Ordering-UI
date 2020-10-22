ae56d09edff90331e5892bdad88e0bf3e0149bb4
carlos quiroz <carloseqv29596@gmail.com>
2020-10-21 21:54:44 -0400

    [FEAT] improve login form design

--- src/components/LoginForm/styles.js
+++ src/components/LoginForm/styles.js
@@ -1,135 +1,147 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const LoginContainer = styled.div`
-  display: flex;
   width: 100%;
+  height: 100%;
+  display: flex;
 
-  button{
-    box-sizing: content-box;
-  }
-
-  .popup {
-    font-size: 0.9em;
-    input{
-      width: 80%
-    }
-  }
-
-  @media (max-width: 980px) {
+  @media (max-width: 768px) {
     flex-direction: column;
-    height: auto;
   }
 `
-
-export const HeroSide = styled.div`
-  width: 50%;
+const Side = styled.div`
   display: flex;
+  width: 50%;
+
+  @media (min-width: 1400px) {
+    height: calc(100vh - 65px);
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`
+export const HeroSide = styled(Side)`
   justify-content: center;
   align-items: center;
   color: ${props => props.theme.colors.primaryContrast};
-  background: ${props => props.theme.colors.primary};
-  z-index: 100;
-  text-align: center;
-  flex: 1.2;
-  max-height: 100%;
-  font-size: 1.7em;
+  background-color:  ${props => props.theme.colors.primary};
 
-  @media (max-width: 980px) {
-    width: 100%;
-    flex: 1;
-    min-height: 300px;
+  @media (max-width: 768px) {
+    height: calc(50vh - 65px);
   }
 `
 
 export const TitleHeroSide = styled.div`
-  z-index: 10;
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  justify-content: center;
-  font-size: 1em;
-  flex: 1.2;
-  padding: 0 50px;
-  @media (max-width: 980px) {
-    flex: 1;
-    padding: 0 10px;
-  }
-  @media (max-width: 480px) {
-    font-size: 0.8em;
-  }
-`
-
-export const FormSide = styled.div`
-  width: 50%;
+  width: 80%;
   display: flex;
   flex-direction: column;
   align-items: center;
   text-align: center;
-  justify-content: space-around;
-  padding: 30px 20px;
-  flex: 1;
-  height: auto;
-  font-size: 1.2em;
 
-  > img {
-    margin-top: 0;
+  h1 {
+    font-size: 52px;
+  }
+
+  h1,
+  p {
+    margin: 0px;
+    color: #FFF;
+  }
+
+  @media (max-width: 576px) {
+    h1 {
+      font-size: 32px;
+    }
+  }
+`
+
+export const FormSide = styled(Side)`
+  flex-direction: column;
+  align-items: center;
+
+  img {
     width: 200px;
   }
 
-  a {
-    text-decoration: none;
-    color: ${props => props.theme.colors.primary};
+  @media (max-width: 1399px) {
+    margin: 0px 0px 15px;
   }
 
-  &.popup{
-    padding: 30px 0;
-    padding-left: 5px;
-  }
-
-  @media (max-width: 980px) {
-    font-size: 1em;
-    height: 100%;
-    width: 100%;
-
-    > img {
-      width: 139px;
-      margin-bottom: 15px;
-    }
-  }
-
-`
-
-export const NewOnPlatform = styled.div`
-  &.popup{
-    font-size: 0.8em
+  @media (max-width: 768px) {
+    margin-top: 20px;
   }
 `
 
-export const SocialIcons = styled.div`
+export const LoginWith = styled.div`
+  @media (max-width: 360px) {
+    font-size: 14px;
+  }
+`
+
+export const FormInput = styled.form`
+  width: 70%;
   display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  width: 100%;
   flex-direction: column;
 
-  > * {
-    color: ${props => props.theme.colors.primary}
+  * {
+    padding: 8px 20px;
+    margin: 10px;
+  }
+
+  input:not(:last-child) {
+    margin-top: 10px;
+    margin-bottom: 0px;
+  }
+
+  button {
+    margin-top: 10px;
+    padding: 5px;
+  }
+
+  @media (max-width: 480px) {
+    width: 90%;
+  }
+`
+
+export const RedirectLink = styled.div`
+  padding: 10px;
+  margin: 10px 0px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  * {
+    margin: 0px;
+    padding: 0px;
+  }
+
+  ${({ register }) => register && css`
+    width: calc(70% - 20px);
+
+    @media (max-width: 480px) {
+      width: calc(90% - 20px);
+    }
+  `}
+
+  @media (max-width: 360px) {
+    flex-direction: column;
   }
 `
 
 export const SocialButtons = styled.div`
-   width: 100%;
+   width: 70%;
    margin-top: 5px;
 
-  > * {
-    margin-top: 5px;
-    margin-bottom: 20px;
-    width: 55%;
-    padding: 5px;
-    color: black;
-  }
+  button {
+    width: 100%;
+    padding: 3px;
+    color: #000000;
 
-  button{
+    img {
+      width: 30px;
+    }
+
     span{
       font-size: 0.8em;
     }
@@ -144,76 +156,7 @@ export const SocialButtons = styled.div`
     }
   }
 
-  @media (max-width: 1024px){
-    > * {
-      width: 80%;
-    }
+  @media (max-width: 480px) {
+    width: 90%;
   }
-
-  @media (max-width: 980px) {
-    > * {
-      margin-top: 5px;
-      width: 55%;
-    }
-  }
-`
-
-export const LoginWith = styled.div`
-  > div {
-    justify-content: center;
-  }
-
-  font-size: 1.1em;
-
-  @media (max-width: 992px) {
-    font-size: 0.8em;
-  }
-`
-
-export const FormInput = styled.form`
-  width: 100%;
-
-  > * {
-    margin: 10px;
-    width: 55%;
-    padding: 10px 20px
-  }
-
-  > button {
-    margin-top: 10px;
-    width: 60%;
-    padding: 5px;
-  }
-
-  &.popup{
-    > button{
-      width: 85%
-    }
-  }
-
-  @media (max-width: 1024px){
-    > * {
-      width: 80%;
-    }
-    button {
-      width: 85%;
-    }
-  }
-
-  @media (max-width: 980px) {
-    > * {
-      margin: 5px;
-      width: 55%;
-    }
-
-    > button{
-      width: 60%;
-      box-sizing: border-box;
-    }
-  }
-`
-
-export const ForgotPassword = styled.div`
-  margin: 10px auto;
-  width: auto;
 `
