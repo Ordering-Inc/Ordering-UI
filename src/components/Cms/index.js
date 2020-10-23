6df905b48ea0c3871e818b408d8321326b06ce40
Jesus Perez <jesusdpch00@gmail.com>
2020-10-23 15:18:56 -0400

    [FIX] design of Skeletons

--- src/components/Cms/index.js
+++ src/components/Cms/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { CmsContent, useLanguage } from 'ordering-components'
-import { CmsContainer } from './styles'
+import { CmsContainer, SkeletonContainer, SkeletonHeader, SkeletonContent, SkeletonInformation, SkeletonSide } from './styles'
 import { NotFoundSource } from '../NotFoundSource'
 import Skeleton from 'react-loading-skeleton'
 const CmsUI = (props) => {
@@ -15,7 +15,24 @@ const CmsUI = (props) => {
   return (
     <CmsContainer>
       {
-        loading && <Skeleton width='100%' height='500px' />
+        loading && (
+          <SkeletonContainer>
+            <SkeletonHeader>
+              <Skeleton width='100%' height='100%' />
+            </SkeletonHeader>
+            <SkeletonContent>
+              <SkeletonInformation>
+                <Skeleton width='100%' height='100px' />
+                <Skeleton width='100%' height='100px' />
+                <Skeleton width='100%' height='100px' />
+                <Skeleton width='100%' height='100px' />
+              </SkeletonInformation>
+              <SkeletonSide>
+                <Skeleton width='100%' height='100%' />
+              </SkeletonSide>
+            </SkeletonContent>
+          </SkeletonContainer>
+        )
       }
       {
         body && (
@@ -26,7 +43,7 @@ const CmsUI = (props) => {
         )
       }
       {
-        (!loading && error) &&
+        (loading && error) &&
           <NotFoundSource
             content={t('ERROR_PAGE', 'Sorry, the selected page was not found.')}
             btnTitle={t('PAGE_REDIRECT', 'Go to pages list')}
