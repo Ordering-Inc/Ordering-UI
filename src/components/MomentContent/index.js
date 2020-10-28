570c83434cb6638cda2abded21269c4be1b8a27a
carlos quiroz <carloseqv29596@gmail.com>
2020-10-28 18:47:35 -0400

    [FEAT] add loading for moment popover/modal

--- src/components/MomentContent/index.js
+++ src/components/MomentContent/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
-import { Container } from './styles'
+import { useOrder } from 'ordering-components'
+
+import { Container, Layer } from './styles'
+
 import { MomentControl } from '../MomentControl'
+import { SpinnerLoader } from '../SpinnerLoader'
+import { useWindowSize } from '../../hooks/useWindowSize'
 
 export const MomentContent = (props) => {
+  const [orderState] = useOrder()
   const currentDate = new Date()
   currentDate.setTime(currentDate.getTime() + (6 * 24 * 60 * 60 * 1000))
   currentDate.setHours(23)
@@ -11,9 +17,23 @@ export const MomentContent = (props) => {
     maxDate: currentDate
   }
 
+  const { width } = useWindowSize()
+  const momentControl = document?.getElementById('moment_control')?.getBoundingClientRect()
+
   return (
-    <Container>
+    <Container isLoading={orderState?.loading}>
       <MomentControl {...momentProps} />
+      {orderState?.loading && (
+        <Layer height={momentControl?.height && `${momentControl?.height}px`}>
+          <SpinnerLoader
+            style={{
+              top: width <= 768 ? '50%' : '40%',
+              position: width <= 768 ? 'absolute' : 'sticky',
+              height: 'auto'
+            }}
+          />
+        </Layer>
+      )}
     </Container>
   )
 }
