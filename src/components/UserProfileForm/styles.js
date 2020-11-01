a257a7a4a1be7caa5ad8e0ecfb7ba5f54b6c916e
Jesus Perez <jesusdpch00@gmail.com>
2020-11-01 15:57:35 -0400

    [FIX] margin bottom for addressList in UserProfile

--- src/components/UserProfileForm/styles.js
+++ src/components/UserProfileForm/styles.js
@@ -159,6 +159,7 @@ export const SavedPlaces = styled.div`
     text-align: center;
     width: 100%;
     margin-top: 40px;
+    margin-bottom: 20px;
       > div{
       display: flex;
       flex-direction: column-reverse;
