99cef0a734e51be684fa91662c4277bf6c293639
Jesus Perez <jesusdpch00@gmail.com>
2020-11-15 18:08:02 -0400

    [FIX] hide of searchbar and category filters

--- src/components/BusinessProductsListing/index.js
+++ src/components/BusinessProductsListing/index.js
@@ -153,7 +153,7 @@ const BusinessProductsListingUI = (props) => {
               <BusinessBasicInformation
                 businessState={businessState}
               />
-              {categoryState.products.length !== 0 && (
+              {(categoryState.products.length !== 0 || searchValue) && (
                 <WrapperSearch>
                   <SearchBar
                     onSearch={handleChangeSearch}
@@ -162,7 +162,7 @@ const BusinessProductsListingUI = (props) => {
                   />
                 </WrapperSearch>
               )}
-              {categoryState.products.length !== 0 && (
+              {!(business.categories.length === 0 && !categoryId) && (
                 <BusinessProductsCategories
                   categories={[{ id: null, name: t('ALL', 'All') }, ...business.categories.sort((a, b) => a.rank - b.rank)]}
                   categorySelected={categorySelected}
