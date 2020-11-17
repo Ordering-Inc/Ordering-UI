32f962e2e447a3889d54527186b5e293493f612c
carlos quiroz <carloseqv29596@gmail.com>
2020-11-17 10:30:34 -0400

    [FEAT] update order bill

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -145,18 +145,24 @@ const CartUI = (props) => {
                   <td>{t('TAX', 'Tax')} ({parseNumber(cart?.business?.tax)}%)</td>
                   <td>{parsePrice(cart?.tax || 0)}</td>
                 </tr>
-                <tr>
-                  <td>{t('DELIVERY_FEE', 'Delivery Fee')}</td>
-                  <td>{parsePrice(cart?.delivery_price || 0)}</td>
-                </tr>
-                <tr>
-                  <td>{t('DRIVER_TIP', 'Driver tip')} ({parseNumber(cart?.driver_tip_rate)}%)</td>
-                  <td>{parsePrice(cart?.driver_tip || 0)}</td>
-                </tr>
-                <tr>
-                  <td>{t('SERVICE_FEE', 'Service Fee')} </td>
-                  <td>{parsePrice(cart?.service_fee || 0)}</td>
-                </tr>
+                {orderState?.options?.type === 1 && cart?.delivery_price > 0 && (
+                  <tr>
+                    <td>{t('DELIVERY_FEE', 'Delivery Fee')}</td>
+                    <td>{parsePrice(cart?.delivery_price)}</td>
+                  </tr>
+                )}
+                {cart?.driver_tip > 0 && (
+                  <tr>
+                    <td>{t('DRIVER_TIP', 'Driver tip')} ({parseNumber(cart?.driver_tip_rate)}%)</td>
+                    <td>{parsePrice(cart?.driver_tip)}</td>
+                  </tr>
+                )}
+                {cart?.service_fee > 0 && (
+                  <tr>
+                    <td>{t('SERVICE_FEE', 'Service Fee')} </td>
+                    <td>{parsePrice(cart?.service_fee)}</td>
+                  </tr>
+                )}
                 {cart?.discount > 0 && (
                   <tr>
                     {cart?.discount_type === 1 ? (
