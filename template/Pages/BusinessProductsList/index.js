c6bb3d302470bda6d1c8949c97b450be747c8e9b
carlos quiroz☕ <carloseqv29596@gmail.com>
2020-11-18 14:30:41 -0400

    [FIX] bug when changes routes (#195)

--- template/Pages/BusinessProductsList/index.js
+++ template/Pages/BusinessProductsList/index.js
@@ -57,19 +57,21 @@ export const BusinessProductsList = (props) => {
     onProductRedirect: ({ slug, category, product }) => {
       if (!category && !product) {
         return window.location.pathname.includes('/store/')
-          ? events.emit('go_to_page', { page: 'business', params: { store: slug } })
-          : events.emit('go_to_page', { page: 'business_slug', params: { store: slug } })
+          ? events.emit('go_to_page', { page: 'business', params: { store: slug }, replace: true })
+          : events.emit('go_to_page', { page: 'business_slug', params: { store: slug }, replace: true })
       }
       return window.location.pathname.includes('/store/')
         ? events.emit('go_to_page', {
           page: 'business',
           params: { store: slug },
-          search: `?category=${category}&product=${product}`
+          search: `?category=${category}&product=${product}`,
+          replace: true
         })
         : events.emit('go_to_page', {
           page: 'business_slug',
           params: { store: slug },
-          search: `?category=${category}&product=${product}`
+          search: `?category=${category}&product=${product}`,
+          replace: true
         })
     },
     onCheckoutRedirect: (cartUuid) => {
--- template/components/ScrollToTop/index.js
+++ template/components/ScrollToTop/index.js
@@ -2,10 +2,9 @@ import React, { useEffect } from 'react'
 import { withRouter } from 'react-router-dom'
 
 export const ScrollToTop = withRouter(({ history, children }) => {
-
   useEffect(() => {
     const unlisten = history.listen((data) => {
-      if (!window.location.search.includes('category')) {
+      if (!window.location.pathname.includes('/store/')) {
         window.scrollTo(0, 0)
       }
     })
