af50da3c52d041bafb43b568839e51f476ce9b93
Jesus David <50583541+YisusJe@users.noreply.github.com>
2020-11-30 20:07:23 -0400

    [ADD] home content to project (#229)
    * [ADD] home content to project
    
    * [FIX] state of homeCms
    
    * [FIX] reassign error value

--- template/Pages/Home/index.js
+++ template/Pages/Home/index.js
@@ -1,39 +1,58 @@
-import React from 'react'
-import { useLanguage } from 'ordering-components'
-import AiOutlineAndroid from '@meronex/icons/ai/AiOutlineAndroid'
-import AiOutlineApple from '@meronex/icons/ai/AiOutlineApple'
+import React, { useEffect, useState } from 'react'
+import { useLanguage, useApi } from 'ordering-components'
 import { HomeHero } from '../../../src/components/HomeHero'
 import { useHistory } from 'react-router-dom'
 import { HelmetTags } from '../../components/HelmetTags'
+import Skeleton from 'react-loading-skeleton'
+import { NotFoundSource } from '../../../src/components/NotFoundSource'
 
 import {
   HomeContainer,
-  HomeSection,
-  HomeTitle,
-  StepsBlock,
-  ImageContent,
-  WrapTextContent,
-  TextContent,
-  Icons,
-  HomeFooter,
-  Line
+  SkeletonContainer,
+  SkeletonHeader,
+  SkeletonContent,
+  SkeletonInformation,
+  SkeletonSide
 } from './styles'
-
-import locationSvg from '../../../template/assets/location-icon.svg'
-import businessSvg from '../../../template/assets/store-icon.svg'
-import productSvg from '../../../template/assets/product-icon.svg'
-import deliverySvg from '../../../template/assets/delivery-icon.svg'
-import burgerImage from '../../../template/assets/burger-image.png'
-import socialMedia from '../../../template/assets/social-media.png'
-import foodBg from '../../../template/assets/food-bg.png'
 
 export const HomePage = (props) => {
   const [, t] = useLanguage()
   const history = useHistory()
+  const [homeState, setHomeState] = useState({ body: null, loading: false, error: null })
+  const [ordering] = useApi()
+  const requestsState = {}
 
   const handlerFindBusiness = () => {
     history.push('/search')
   }
+
+  const getPage = async () => {
+    setHomeState({ ...homeState, loading: true })
+    try {
+      const source = {}
+      requestsState.page = source
+      const { content: { error, result } } = await ordering.pages('orderingHome').get({ cancelToken: source })
+      setHomeState({ ...homeState, loading: false })
+      if (!error) {
+        setHomeState({ ...homeState, body: result.body })
+      } else {
+        setHomeState({ ...homeState, error: result })
+      }
+    } catch (err) {
+      if (err.constructor.name !== 'Cancel') {
+        setHomeState({ ...homeState, loading: false, error: [err.message] })
+      }
+    }
+  }
+
+  useEffect(() => {
+    getPage()
+    return () => {
+      if (requestsState.page) {
+        requestsState.page.cancel()
+      }
+    }
+  }, [])
 
   return (
     <>
@@ -43,80 +62,40 @@ export const HomePage = (props) => {
           {...props}
           onFindBusiness={handlerFindBusiness}
         />
-        <HomeSection bgColor='#F8F8F8' column>
-          <HomeTitle>
-            <h2>
-              {t('STEPS_TO_PLACE', 'Steps to place')} <span>{t('STEPS_TITLE_HIGHLIGHT', 'an order')}</span>
-            </h2>
-          </HomeTitle>
-          <StepsBlock>
-            <div>
-              <img src={locationSvg} alt='location' width='88' height='88' />
-              <h3>{t('ADD_ADDRESS', 'Add Address')}</h3>
-            </div>
-            <Line />
-            <div>
-              <img src={businessSvg} alt='business' width='88' height='88' />
-              <h3>{t('SELECT_AN_BUSINESS', 'Select your Business')}</h3>
-            </div>
-            <Line />
-            <div>
-              <img src={productSvg} alt='product' width='88' height='88' />
-              <h3>{t('SELECT_PRODUCTS', 'Choose your Products')}</h3>
-            </div>
-            <Line />
-            <div>
-              <img src={deliverySvg} alt='delivery' width='88' height='88' />
-              <h3>{t('PACKAGE_ARRIVES', 'Your package arrives')}</h3>
-            </div>
-          </StepsBlock>
-        </HomeSection>
-        <HomeSection>
-          <ImageContent>
-            <div>
-              <img src={burgerImage} alt='img' width='391' height='400' />
-            </div>
-          </ImageContent>
-          <TextContent>
-            <WrapTextContent>
-              <div>
-                <h2>
-                  {t('HOME_CONTENT_TITLE', 'We deliver more than')} <span>{t('HOME_CONTENT_TITLE_HIGHLIGHT', 'food')}</span>
-                </h2>
-                <p>{t('TEXT_CONTENT_PARAGRAPH_1', 'With the largest on-demand network in the industry, you can explore your city, find its hidden hotspots, and watch as we bring your new favorite right to your door.')}</p>
-                <p>{t('HOME_CONTENT_PARAGRAPH_2', 'Download the app for iOS or Android for free.')}</p>
-              </div>
-            </WrapTextContent>
-          </TextContent>
-        </HomeSection>
-
-        <HomeSection bgColor='#F8F8F8' className='reverse'>
-          <TextContent>
-            <WrapTextContent>
-              <div>
-                <h2>{t('APPS_TITLE', 'Download')} <span>{t('APPS_TITLE_HIGHLIGHT', 'our apps')}</span></h2>
-                <p>{t('APPS_PARAGRAPH_1', 'The best ordering experience on your smartphone.')}</p>
-                <p>{t('APPS_PARAGRAPH_2', 'For speedy ordering and delivery updates, get the apps now.')}</p>
-              </div>
-              <Icons>
-                <AiOutlineApple />
-                <AiOutlineAndroid />
-              </Icons>
-            </WrapTextContent>
-          </TextContent>
-          <ImageContent>
-            <div>
-              <img src={socialMedia} alt='img' width='295' height='400' />
-            </div>
-          </ImageContent>
-        </HomeSection>
-        <HomeSection bgimage={foodBg}>
-          <TextContent className='center'>
-            <HomeFooter>
-              <h2>{t('HOME_CONTENT_HERO', 'Something for Everyone')}</h2>
-            </HomeFooter>
-          </TextContent>
-        </HomeSection>
+        {
+          homeState.loading && (
+            <SkeletonContainer>
+              <SkeletonHeader>
+                <Skeleton width='100%' height='100%' />
+              </SkeletonHeader>
+              <SkeletonContent>
+                <SkeletonInformation>
+                  <Skeleton width='100%' height='100px' />
+                  <Skeleton width='100%' height='100px' />
+                  <Skeleton width='100%' height='100px' />
+                  <Skeleton width='100%' height='100px' />
+                </SkeletonInformation>
+                <SkeletonSide>
+                  <Skeleton width='100%' height='100%' />
+                </SkeletonSide>
+              </SkeletonContent>
+            </SkeletonContainer>
+          )
+        }
+        {
+          homeState.body && (
+            <div dangerouslySetInnerHTML={{
+              __html: homeState.body
+            }}
+            />
+          )
+        }
+        {
+          (!homeState.loading && homeState.error) &&
+            <NotFoundSource
+              content={t('ERROR_HOME', 'Ups... an error has occured')}
+            />
+        }
       </HomeContainer>
     </>
   )
