32678d48bfc6ca89301a824cd9c6851eafff4e29
Jesus Perez <jesusdpch00@gmail.com>
2020-12-15 18:31:47 -0400

    [FEAT] getTraduction function

--- src/utils/index.js
+++ src/utils/index.js
@@ -107,3 +107,20 @@ export const getGoogleMapImage = ({ lat, lng }) => {
  * @param {Array} arr to flat
  */
 export const flatArray = arr => [].concat(...arr)
+
+/**
+ * Function to return the traduction depending of a key 't'
+ * @param {string} key for traduction
+ */
+export const getTraduction = key => {
+  const keyList = {
+    // Add the key and traduction that you need below
+    ERROR_ORDER_WITHOUT_CART: 'The order was placed without a cart'
+  }
+
+  if (keyList[key]) {
+    return keyList[key]
+  } else {
+    return key
+  }
+}
