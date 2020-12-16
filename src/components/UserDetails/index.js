32b406127d4e60f5866f0a1fd8a476be44e93e3a
Jesus Perez <jesusdpch00@gmail.com>
2020-12-16 16:17:38 -0400

    [FIX] value of minLength password for some components

--- src/components/UserDetails/index.js
+++ src/components/UserDetails/index.js
@@ -259,7 +259,7 @@ const UserDetailsUI = (props) => {
                     ref={register({
                       required: isRequiredField('password') ? t('VALIDATION_ERROR_PASSWORD_REQUIRED', 'The field Password is required').replace('_attribute_', t('PASSWORD', 'Password')) : null,
                       minLength: {
-                        value: 5,
+                        value: 8,
                         message: t('VALIDATION_ERROR_PASSWORD_MIN_STRING', 'The Password must be at least 8 characters.').replace('_attribute_', t('PASSWORD', 'Password')).replace('_min_', 8)
                       }
                     })}
--- src/components/UserProfileForm/index.js
+++ src/components/UserProfileForm/index.js
@@ -301,7 +301,7 @@ const UserProfileFormUI = (props) => {
                       ref={register({
                         required: isRequiredField('password') ? t('VALIDATION_ERROR_PASSWORD_REQUIRED', 'The field Password is required').replace('_attribute_', t('PASSWORD', 'Password')) : null,
                         minLength: {
-                          value: 5,
+                          value: 8,
                           message: t('VALIDATION_ERROR_PASSWORD_MIN_STRING', 'The Password must be at least 8 characters.').replace('_attribute_', t('PASSWORD', 'Password')).replace('_min_', 8)
                         }
                       })}
