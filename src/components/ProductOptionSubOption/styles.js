0106a00cfa2c718e9902e6724a25e4d6e51d616b
Jesus Perez <jesusdpch00@gmail.com>
2020-12-22 12:19:19 -0400

    [FIX] rtl of max limit error

--- src/components/ProductOptionSubOption/styles.js
+++ src/components/ProductOptionSubOption/styles.js
@@ -33,6 +33,7 @@ export const IconControl = styled.div`
 `
 export const Text = styled.div`
   flex: 1 1 auto;
+  display: flex;
   font-weight: 300;
   color: #555;
   white-space: nowrap;
@@ -40,10 +41,23 @@ export const Text = styled.div`
   text-overflow: ellipsis;
   margin: 0 5px;
 
+  div {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
   span {
     margin-left: 10px;
     font-weight: 500;
     color: ${props => props.theme.colors.primary};
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    ${props => props.theme?.rtl && css`
+      margin-left: 0;
+      margin-right: 10px;    
+    `}
   }
 `
 
