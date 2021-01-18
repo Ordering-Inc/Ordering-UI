23e2956604793cc2ec10e343ba8108e1dfeb168b
carlos quiroz <carlos.q@ordering.co>
2021-01-18 12:19:48 -0400

    [FEAT] testing for not network connectivity

--- src/components/NotNetworkConnectivity/styles.js
+++ src/components/NotNetworkConnectivity/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  display: flex;
+  display: ${({ isOnline }) => isOnline ? 'none' : 'flex'};
   flex-direction: column;
   justify-content: center;
   align-items: center;
