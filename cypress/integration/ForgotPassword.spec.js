f3f49cf678e04f058681406e41aba32141aede7d
Jesus Perez <jesus@ordering.co>
2021-01-18 14:27:46 -0400

    [FEAT] update tests

--- cypress/integration/ForgotPassword.spec.js
+++ cypress/integration/ForgotPassword.spec.js
@@ -5,14 +5,13 @@ context('Forgot Password', () => {
 
   it('Check UI', () => {
     cy.server({ method: 'POST' })
-    cy.get('img').should('be.visible')
-    cy.get('[type="email"]').should('be.visible')
-    cy.get('button').contains('PASSWORD').should('be.visible')
+    cy.get('input').should('be.visible')
+    cy.get('button').contains('Recover Password').should('be.visible')
   })
 
   it('Recover Password', () => {
     cy.server({ method: 'POST' })
-    cy.get('[type="email"]').type('superadmin@ordering.co')
-    cy.get('button').contains('PASSWORD').click()
+    cy.get('input').type('superadmin@ordering.co')
+    cy.get('button').contains('Recover Password').click()
   })
 })
