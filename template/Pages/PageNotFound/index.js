bd7e4f9272b4837e1f6362ec9e491ccd039c3ea3
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2021-01-19 10:59:35 -0400

    Delete template/Pages directory

--- template/Pages/PageNotFound/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { PageNotFound as PageNotFoundController } from '../../../src/components/PageNotFound'
-
-export const PageNotFound = (props) => {
-  return <PageNotFoundController {...props} />
-}
