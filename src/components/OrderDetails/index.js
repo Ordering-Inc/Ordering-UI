2c4895b90afa23c198bf03ebe1d68231e638a720
Jesus Perez <jesus@ordering.co>
2021-01-22 12:34:18 -0400

    [FIX] subtotal

--- src/components/OrderDetails/index.js
+++ src/components/OrderDetails/index.js
@@ -255,7 +255,7 @@ const OrderDetailsUI = (props) => {
                 <tbody>
                   <tr>
                     <td>{t('SUBTOTAL', 'Subtotal')}</td>
-                    <td>{parsePrice(order?.subtotal)}</td>
+                    <td>{order?.tax_type === 1 ? parsePrice(order?.subtotal - order?.totalTax) : parsePrice(order?.subtotal)}</td>
                   </tr>
                   <tr>
                     <td>{order?.tax_type === 1 ? t('TAX_INCLUDED', 'Tax (included)') : t('TAX', 'Tax')} ({parseNumber(order?.tax)}%)</td>
