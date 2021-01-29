c63c660f106b9e0627f6e0b09062e5f7d1cf0ead
Jesus Perez <jesus@ordering.co>
2021-01-28 21:40:07 -0400

    [add] clear button for searchBar

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,12 +1,15 @@
 import React, { useRef, useEffect } from 'react'
 
 import { Input } from '../../styles/Inputs'
+import { useTheme } from '../../contexts/ThemeContext'
 
 import {
-  BusinessSearch
+  BusinessSearch,
+  DeleteContent
 } from './styles'
 
 export const SearchBar = ({ onSearch, search, placeholder, lazyLoad }) => {
+  const [theme] = useTheme()
   let timeout = null
   let previousSearch
   const el = useRef()
@@ -26,6 +29,10 @@ export const SearchBar = ({ onSearch, search, placeholder, lazyLoad }) => {
     previousSearch = e.target.value
   }
 
+  const handleClear = () => {
+    onSearch('')
+  }
+
   useEffect(() => {
     el.current.onkeyup = onChangeSearch
   }, [])
@@ -37,7 +44,7 @@ export const SearchBar = ({ onSearch, search, placeholder, lazyLoad }) => {
   }, [search])
 
   return (
-    <BusinessSearch className='search-bar'>
+    <BusinessSearch className='search-bar' hasValue={el.current?.value}>
       <Input
         ref={el}
         name='search'
@@ -46,6 +53,10 @@ export const SearchBar = ({ onSearch, search, placeholder, lazyLoad }) => {
         autoComplete='off'
         maxLength='500'
       />
+      <DeleteContent>
+        {el.current?.value ? <span onClick={handleClear}>Clear</span> : <img src={theme?.images?.general?.searchIcon}/>}
+      </DeleteContent>
+     
     </BusinessSearch>
   )
 }
