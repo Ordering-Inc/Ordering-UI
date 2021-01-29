5f92424e20fe7adc90f0a264e171563f6a04d596
carlos quiroz <carlos.q@ordering.co>
2021-01-28 20:49:31 -0400

    [FEAT] improve payment method cash component

--- src/components/PaymentOptionCash/index.js
+++ src/components/PaymentOptionCash/index.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react'
-import { useForm } from 'react-hook-form'
+import React, { useEffect, useState } from 'react'
 import { useUtils, useLanguage } from 'ordering-components'
 
 import {
@@ -18,62 +17,38 @@ export const PaymentOptionCash = (props) => {
     setErrorCash
   } = props
   const [, t] = useLanguage()
-
-  const { handleSubmit, register, errors } = useForm()
   const [{ parsePrice }] = useUtils()
+
+  const [value, setvalue] = useState(null)
 
   const handleChangeCash = (e) => {
     let cash = parseFloat(e?.target?.value)
     cash = isNaN(cash) ? null : cash
+    setvalue(cash)
     onChangeData && onChangeData({ cash })
-    handleSubmit(() => {})(e)
   }
 
-  /**
-   * effect for disable the place button on component did mount
-   */
   useEffect(() => {
-    handleChangeCash()
-  }, [])
-
-  /**
-   * effect for disable the place button on errors with cash
-   */
-  useEffect(() => {
-    handleError()
-  }, [errors])
-
-  const handleError = () => {
-    if (errors.cash) {
+    if (value && parseFloat(value) < orderTotal) {
       setErrorCash && setErrorCash(true)
     } else {
       setErrorCash && setErrorCash(false)
     }
-  }
+  }, [value, orderTotal])
 
   return (
     <PaymentCashContainer>
-      <FormCash onSubmit={handleSubmit(() => {})}>
+      <FormCash>
         <WrapperInput>
           <label>{t('NOT_EXACT_CASH_AMOUNT', 'Don\'t have exact amount? Let us know with how much will you pay')}</label>
           <Input
             name='cash'
-            type='number'
+            type='text'
             placeholder='0'
             onChange={handleChangeCash}
-            ref={
-              register({
-                validate: value => {
-                  return !value || value >= orderTotal
-                }
-              })
-            }
           />
         </WrapperInput>
-        {errors.cash && errors.cash.type === 'required' && (
-          <ErrorText>{t('FIELD_REQUIRED', 'This field is required')}</ErrorText>
-        )}
-        {errors.cash && errors.cash.type === 'validate' && (
+        {value && parseFloat(value) < orderTotal && (
           <ErrorText>{t('VALUE_GREATER_THAN_TOTAL', 'This value must be greater than order total')}: {parsePrice(orderTotal)}</ErrorText>
         )}
       </FormCash>
