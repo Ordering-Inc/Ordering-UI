ff2d2b85657fc058184b835ccc753f1d6d9b7530
Jesus Perez <jesus@ordering.co>
2021-02-25 20:28:56 -0400

    [FIX] cannot read prop lat of undefined

--- src/utils/index.js
+++ src/utils/index.js
@@ -88,8 +88,8 @@ export const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1)
  * Function to return a static google maps image based in location
  * @param {object} param object with latitude and logitude
  */
-export const getGoogleMapImage = ({ lat, lng }, apiKey) => {
-  return `https://maps.googleapis.com/maps/api/staticmap?size=500x190&center=${lat},${lng}&zoom=17&scale=2&maptype=roadmap&&markers=icon:https://res.cloudinary.com/ditpjbrmz/image/upload/f_auto,q_auto,w_45,q_auto:best,q_auto:best/v1564675872/marker-customer_kvxric.png%7Ccolor:white%7C${lat},${lng}&key=${apiKey}`
+export const getGoogleMapImage = (location, apiKey) => {
+  return `https://maps.googleapis.com/maps/api/staticmap?size=500x190&center=${location?.lat},${location?.lng}&zoom=17&scale=2&maptype=roadmap&&markers=icon:https://res.cloudinary.com/ditpjbrmz/image/upload/f_auto,q_auto,w_45,q_auto:best,q_auto:best/v1564675872/marker-customer_kvxric.png%7Ccolor:white%7C${location?.lat},${location?.lng}&key=${apiKey}`
 }
 
 /**
