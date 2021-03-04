7c2224a6f6ce38acc496c0d22b226153601718bc
Carlos Quiroz <carlos.q@ordering.co>
2021-03-03 22:36:39 -0400

    change

--- template/pages/ResetPassword/index.js
+++ template/pages/ResetPassword/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { HelmetTags } from '../../components/HelmetTags'
-import { ResetPassword as ResetPasswordController } from 'ordering-ui'
 import { useEvent } from 'ordering-components'
+import { HelmetTags } from '../../components/HelmetTags'
+import { ResetPassword as ResetPasswordController } from '../../../src/components/ResetPassword'
 
 export const ResetPassword = (props) => {
   const [events] = useEvent()
