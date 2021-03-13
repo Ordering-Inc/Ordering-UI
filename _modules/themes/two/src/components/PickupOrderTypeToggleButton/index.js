b4228690aa84c94ba30035f655bf6168b76879b1
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2021-03-14 00:17:40 +0800

    fix empty page

--- _modules/themes/two/src/components/PickupOrderTypeToggleButton/index.js
+++ _modules/themes/two/src/components/PickupOrderTypeToggleButton/index.js
@@ -1,17 +1,21 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.PickupOrderTypeToggleButton = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 
 var _orderingComponents = require("ordering-components");
 
 var _Buttons = require("../../styles/Buttons");
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
@@ -32,7 +36,8 @@ function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 var PickupOrderTypeToggleButtonUI = function PickupOrderTypeToggleButtonUI(props) {
-  var handleChangeOrderType = props.handleChangeOrderType,
+  var isMainBusinessListingPage = props.isMainBusinessListingPage,
+      handleChangeOrderType = props.handleChangeOrderType,
       typeSelected = props.typeSelected,
       handleCustomClick = props.handleCustomClick;
 
@@ -50,6 +55,13 @@ var PickupOrderTypeToggleButtonUI = function PickupOrderTypeToggleButtonUI(props
     }
   };
 
+  (0, _react.useEffect)(function () {
+    if (!isMainBusinessListingPage) return;
+
+    if (typeSelected !== 1) {
+      handleChangeOrderType(1);
+    }
+  }, [isMainBusinessListingPage]);
   return typeSelected !== undefined && /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement(_Buttons.Button, {
     color: typeSelected === 2 ? 'dark' : 'secondary',
     onClick: function onClick() {
