ec1ce977b2a97c79e75b3619f1dcde97e92f90b7
Jesus Perez <jesus@ordering.co>
2021-03-29 23:19:40 -0400

    [FIX] businessId is required when useOrderContext is true

--- src/components/Checkout/index.js
+++ src/components/Checkout/index.js
@@ -310,20 +310,20 @@ const CheckoutUI = (props) => {
           {!props.isHideSectionFive &&
             !cartState.loading &&
             cart &&
+            cart?.business_id &&
             options.type === 1 &&
             cart?.status !== 2 &&
             validationFields?.fields?.checkout?.driver_tip?.enabled &&
-          (
-            <DriverTipContainer>
-              <h1>{t('DRIVER_TIPS', 'Driver Tips')}</h1>
-              <DriverTips
-                businessId={cart?.business_id}
-                driverTipsOptions={DriverTipsOptions}
-                useOrderContext
-              />
-            </DriverTipContainer>
-          )}
-
+            (
+              <DriverTipContainer>
+                <h1>{t('DRIVER_TIPS', 'Driver Tips')}</h1>
+                <DriverTips
+                  businessId={cart?.business_id}
+                  driverTipsOptions={DriverTipsOptions}
+                  useOrderContext
+                />
+              </DriverTipContainer>
+            )}
           {props.beforeElementsSectionSix?.map((BeforeElement, i) => (
             <React.Fragment key={i}>
               {BeforeElement}
