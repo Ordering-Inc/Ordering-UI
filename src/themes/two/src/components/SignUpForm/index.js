bedb5dda93f67097ee87aaf9ee44e28384edc639
Jesus Perez <jesus@ordering.co>
2021-04-26 19:20:05 -0400

    [FIX] inputs show

--- src/themes/two/src/components/SignUpForm/index.js
+++ src/themes/two/src/components/SignUpForm/index.js
@@ -199,7 +199,7 @@ const SignUpFormUI = (props) => {
               <>
                 {
                   validationFields?.fields?.checkout && Object.values(validationFields?.fields?.checkout).map(field => !notValidationFields.includes(field.code) && (
-                    showField && showField(field.code) && field.enabled && field.required && (
+                    showField && showField(field.code) && (
                       <InputGroup key={field.id}>
                         <label>{t(field.name)}</label>
                         <Input
