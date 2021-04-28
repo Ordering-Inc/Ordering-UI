9818c6906427f8be84a35cdf29dc12d0314afdad
carlosq-ordering <carlos.q@ordering.co>
2021-04-27 23:05:13 -0400

    [FEAT] implement driver tips custom

--- src/themes/two/src/components/DriverTips/styles.js
+++ src/themes/two/src/components/DriverTips/styles.js
@@ -2,27 +2,75 @@ import styled from 'styled-components'
 
 export const DriverTipContainer = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
+  flex-wrap: wrap;
   margin-top: 20px;
-  background: ${props => props.theme.colors.secondary};
-  border-radius: 25px;
-  height: 40px;
+  width: 100%;
 `
 
 export const TipCard = styled.div`
-  flex: 1;
   display: flex;
   justify-content: center;
   align-items: center;
-  border-radius: 25px;
+  border-radius: 100%;
+  padding: 10px;
+  border: 1px solid ${props => props.theme.colors.primary};
   cursor: pointer;
   text-transform: capitalize;
-  height: 100%;
+  height: 30px;
+  min-width: 30px;
   font-size: 14px;
-  width: 25%;
 
   &.active {
-    background: ${props => props.theme.colors.darkTextColor};
-    color: #fff;
+    background-color: ${props => props.theme.colors.primary};
+    color: ${props => props.theme.colors.primaryContrast};
   }
+
+  @media (min-width: 768px) {
+    border-radius: 16px;
+    padding: 5px 30px;
+  }
+`
+
+export const FormDriverTip = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const DriverTipLabel = styled.label`
+  font-size: 16px;
+  align-self: flex-start;
+
+  ${props => props.theme?.rtl && css`
+    margin-left: 20px;
+    margin-right: 0;
+  `}
+`
+
+export const WrapperInput = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-top: 10px;
+
+  input {
+    width: 65%;
+    padding: 4px 15px;
+    box-sizing: border-box;
+  }
+
+  @media (min-width: 768px) {
+    input {
+      width: 80%;
+    }
+  }
+`
+
+export const DriverTipMessage = styled.p`
+  color: ${props => props.theme.colors.primary};
+  font-weight: 600;
 `
