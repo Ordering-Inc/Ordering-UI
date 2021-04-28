e545524c4b15189b59afa3801146a66437bf61f7
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2021-04-27 23:07:07 -0400

    [FEAT] implement driver tips custom (#515)

--- src/components/DriverTips/styles.js
+++ src/components/DriverTips/styles.js
@@ -5,6 +5,7 @@ export const DriverTipContainer = styled.div`
   justify-content: space-around;
   flex-wrap: wrap;
   margin-top: 20px;
+  width: 100%;
 `
 
 export const TipCard = styled.div`
@@ -17,7 +18,7 @@ export const TipCard = styled.div`
   cursor: pointer;
   text-transform: capitalize;
   height: 30px;
-  width: 30px;
+  min-width: 30px;
   font-size: 14px;
 
   &.active {
@@ -29,4 +30,47 @@ export const TipCard = styled.div`
     border-radius: 16px;
     padding: 5px 30px;
   }
+`
+
+export const FormDriverTip = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const DriverTipLabel = styled.label`
+  font-size: 16px;
+  align-self: flex-start;
+
+  ${props => props.theme?.rtl && css`
+    margin-left: 20px;
+    margin-right: 0;
+  `}
+`
+
+export const WrapperInput = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-top: 10px;
+
+  input {
+    width: 65%;
+    padding: 4px 15px;
+    box-sizing: border-box;
+  }
+
+  @media (min-width: 768px) {
+    input {
+      width: 80%;
+    }
+  }
+`
+
+export const DriverTipMessage = styled.p`
+  color: ${props => props.theme.colors.primary};
+  font-weight: 600;
 `
