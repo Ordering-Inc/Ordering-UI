94a60d9322c61e03b48839013e0a3683c1e9438a
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2021-05-03 18:55:01 -0400

    [FEAT] add validations for driover tips custom (#536)

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -205,8 +205,8 @@ const CartUI = (props) => {
                       <td>
                         {t('DRIVER_TIP', 'Driver tip')}
                         {cart?.driver_tip_rate > 0 &&
-                          configs?.driver_tip_type?.value === 2 &&
-                          !!!configs?.driver_tip_use_custom?.value &&
+                          parseInt(configs?.driver_tip_type?.value, 10) === 2 &&
+                          !!!parseInt(configs?.driver_tip_use_custom?.value, 10) &&
                         (
                           <span>{`(${verifyDecimals(cart?.driver_tip_rate, parseNumber)}%)`}</span>
                         )}
