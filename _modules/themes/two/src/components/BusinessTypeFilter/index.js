8981c934d608696a840e4a27137a43a57b9af00d
carlosq-ordering <carlos.q@ordering.co>
2021-05-07 18:10:01 -0400

    build

--- _modules/themes/two/src/components/BusinessTypeFilter/index.js
+++ _modules/themes/two/src/components/BusinessTypeFilter/index.js
@@ -9,6 +9,8 @@ exports.BusinessTypeFilter = void 0;
 
 var _react = _interopRequireWildcard(require("react"));
 
+var _reactLoadingSkeleton = _interopRequireDefault(require("react-loading-skeleton"));
+
 var _orderingComponents = require("ordering-components");
 
 var _Tabs = require("../../styles/Tabs");
@@ -19,15 +21,25 @@ var _AutoScroll = require("../AutoScroll");
 
 var _styledComponents = require("styled-components");
 
-function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
 
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
 
@@ -37,25 +49,33 @@ function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o =
 
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
-function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+function _iterableToArrayLimit(arr, i) { var _i = arr && (typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]); if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 var BusinessTypeFilterUI = function BusinessTypeFilterUI(props) {
-  var images = props.images,
-      businessTypes = props.businessTypes,
+  var _theme$images, _theme$images$categor;
+
+  var typesState = props.typesState,
       currentTypeSelected = props.currentTypeSelected,
       handleChangeBusinessType = props.handleChangeBusinessType,
       noAutoScroll = props.noAutoScroll;
+  var loading = typesState.loading,
+      error = typesState.error,
+      types = typesState.types;
 
   var _useLanguage = (0, _orderingComponents.useLanguage)(),
       _useLanguage2 = _slicedToArray(_useLanguage, 2),
       t = _useLanguage2[1];
 
+  var theme = (0, _styledComponents.useTheme)();
+
   var _useState = (0, _react.useState)(false),
       _useState2 = _slicedToArray(_useState, 2),
       load = _useState2[0],
       setLoad = _useState2[1];
+
+  var defaultImage = (_theme$images = theme.images) === null || _theme$images === void 0 ? void 0 : (_theme$images$categor = _theme$images.categories) === null || _theme$images$categor === void 0 ? void 0 : _theme$images$categor.all;
 
   var handleChangeCategory = function handleChangeCategory(category) {
     handleChangeBusinessType && handleChangeBusinessType(category);
@@ -64,122 +84,82 @@ var BusinessTypeFilterUI = function BusinessTypeFilterUI(props) {
   return /*#__PURE__*/_react.default.createElement(_styles.TypeContainer, {
     id: "container",
     noAutoScroll: noAutoScroll
-  }, /*#__PURE__*/_react.default.createElement(_Tabs.Tabs, {
+  }, loading && /*#__PURE__*/_react.default.createElement(_Tabs.Tabs, {
     variant: "primary"
-  }, !noAutoScroll ? /*#__PURE__*/_react.default.createElement(_AutoScroll.AutoScroll, null, businessTypes && businessTypes.length > 0 && businessTypes.map(function (type, i) {
+  }, /*#__PURE__*/_react.default.createElement(_AutoScroll.AutoScroll, null, /*#__PURE__*/_react.default.createElement(_Tabs.Tab, {
+    className: "category",
+    style: styles.wrapperSkeleton
+  }, _toConsumableArray(Array(4)).map(function (_, i) {
+    return /*#__PURE__*/_react.default.createElement(_reactLoadingSkeleton.default, {
+      id: "skeleton",
+      key: i,
+      circle: true,
+      height: 150,
+      width: 150
+    });
+  })))), !loading && !error && types && types.length > 0 && /*#__PURE__*/_react.default.createElement(_Tabs.Tabs, {
+    variant: "primary"
+  }, !noAutoScroll ? /*#__PURE__*/_react.default.createElement(_AutoScroll.AutoScroll, null, types.map(function (type, i) {
     return /*#__PURE__*/_react.default.createElement(_Tabs.Tab, {
-      className: "category",
-      active: type.value === currentTypeSelected,
-      key: type.value
-    }, !type.value || i > images.length - 1 ? /*#__PURE__*/_react.default.createElement(_styles.ImageContainer, {
-      active: type.value === currentTypeSelected,
+      key: type.id,
+      active: type.id === currentTypeSelected,
+      className: "category"
+    }, /*#__PURE__*/_react.default.createElement(_styles.ImageContainer, {
+      active: type.id === currentTypeSelected,
       load: load
     }, /*#__PURE__*/_react.default.createElement("img", {
-      src: images[0].image,
-      alt: "all",
+      src: type.image || defaultImage,
+      alt: type.name.toLowerCase(),
       onLoad: function onLoad() {
         return setLoad(true);
       },
       onClick: function onClick() {
-        return handleChangeCategory(type.value);
+        return handleChangeCategory(type.id);
       },
       width: "150px",
       height: "150px",
       loading: "lazy"
-    })) : '', images.map(function (image) {
-      return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, {
-        key: image.value
-      }, image.value === type.value ? /*#__PURE__*/_react.default.createElement(_styles.ImageContainer, {
-        active: type.value === currentTypeSelected,
-        load: load
-      }, /*#__PURE__*/_react.default.createElement("img", {
-        src: image.image,
-        alt: type.value,
-        onClick: function onClick() {
-          return handleChangeCategory(type.value);
-        },
-        width: "150px",
-        height: "150px",
-        loading: "lazy"
-      })) : '');
-    }), t("BUSINESS_TYPE_".concat(type.value ? type.value.toUpperCase() : 'ALL'), type.key));
-  })) : /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, businessTypes && businessTypes.length > 0 && businessTypes.map(function (type, i) {
+    })), t("BUSINESS_TYPE_".concat(type.name.replace(/\s/g, '_').toUpperCase()), type.name));
+  })) : /*#__PURE__*/_react.default.createElement("div", {
+    style: {
+      display: 'flex',
+      flexDirection: 'row'
+    }
+  }, types.map(function (type, i) {
     return /*#__PURE__*/_react.default.createElement(_Tabs.Tab, {
-      className: "category",
-      active: type.value === currentTypeSelected,
-      key: type.value
-    }, !type.value || i > images.length - 1 ? /*#__PURE__*/_react.default.createElement(_styles.ImageContainer, {
-      noAutoScroll: noAutoScroll,
-      active: type.value === currentTypeSelected,
+      key: type.id,
+      active: type.id === currentTypeSelected,
+      className: "category"
+    }, /*#__PURE__*/_react.default.createElement(_styles.ImageContainer, {
+      active: type.id === currentTypeSelected,
       load: load
     }, /*#__PURE__*/_react.default.createElement("img", {
-      src: images[0].image,
-      alt: "all",
+      src: type.image || defaultImage,
+      alt: type.name.toLowerCase(),
       onLoad: function onLoad() {
         return setLoad(true);
       },
       onClick: function onClick() {
-        return handleChangeCategory(type.value);
+        return handleChangeCategory(type.id);
       },
+      width: "150px",
+      height: "150px",
       loading: "lazy"
-    })) : '', images.map(function (image) {
-      return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, {
-        key: image.value
-      }, image.value === type.value ? /*#__PURE__*/_react.default.createElement(_styles.ImageContainer, {
-        noAutoScroll: noAutoScroll,
-        active: type.value === currentTypeSelected,
-        load: load
-      }, /*#__PURE__*/_react.default.createElement("img", {
-        src: image.image,
-        alt: type.value,
-        onClick: function onClick() {
-          return handleChangeCategory(type.value);
-        },
-        loading: "lazy"
-      })) : '');
-    }), /*#__PURE__*/_react.default.createElement("p", null, t("BUSINESS_TYPE_".concat(type.value ? type.value.toUpperCase() : 'ALL'), type.key)));
+    })), t("BUSINESS_TYPE_".concat(type.name.replace(/\s/g, '_').toUpperCase()), type.name));
   }))));
 };
 
+var styles = {
+  wrapperSkeleton: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'row'
+  }
+};
+
 var BusinessTypeFilter = function BusinessTypeFilter(props) {
-  var _theme$images, _theme$images$categor, _theme$images2, _theme$images2$catego, _theme$images3, _theme$images3$catego, _theme$images4, _theme$images4$catego, _theme$images5, _theme$images5$catego;
-
-  var theme = (0, _styledComponents.useTheme)();
-
   var businessTypeFilterProps = _objectSpread(_objectSpread({}, props), {}, {
     UIComponent: BusinessTypeFilterUI,
-    businessTypes: props.businessTypes || [{
-      key: 'All',
-      value: null
-    }, {
-      key: 'Food',
-      value: 'food'
-    }, {
-      key: 'Groceries',
-      value: 'groceries'
-    }, {
-      key: 'Alcohol',
-      value: 'alcohol'
-    }, {
-      key: 'Laundry',
-      value: 'laundry'
-    }],
-    images: props.images || [{
-      image: (_theme$images = theme.images) === null || _theme$images === void 0 ? void 0 : (_theme$images$categor = _theme$images.categories) === null || _theme$images$categor === void 0 ? void 0 : _theme$images$categor.all,
-      value: 'all'
-    }, {
-      image: (_theme$images2 = theme.images) === null || _theme$images2 === void 0 ? void 0 : (_theme$images2$catego = _theme$images2.categories) === null || _theme$images2$catego === void 0 ? void 0 : _theme$images2$catego.food,
-      value: 'food'
-    }, {
-      image: (_theme$images3 = theme.images) === null || _theme$images3 === void 0 ? void 0 : (_theme$images3$catego = _theme$images3.categories) === null || _theme$images3$catego === void 0 ? void 0 : _theme$images3$catego.groceries,
-      value: 'groceries'
-    }, {
-      image: (_theme$images4 = theme.images) === null || _theme$images4 === void 0 ? void 0 : (_theme$images4$catego = _theme$images4.categories) === null || _theme$images4$catego === void 0 ? void 0 : _theme$images4$catego.alcohol,
-      value: 'alcohol'
-    }, {
-      image: (_theme$images5 = theme.images) === null || _theme$images5 === void 0 ? void 0 : (_theme$images5$catego = _theme$images5.categories) === null || _theme$images5$catego === void 0 ? void 0 : _theme$images5$catego.laundry,
-      value: 'laundry'
-    }],
     defaultBusinessType: props.defaultBusinessType || null,
     onChangeBusinessType: props.handleChangeBusinessType
   });
