530f5ba757cfbf194bb5ff86080f19aadceaa381
Jesus David <50583541+YisusJe@users.noreply.github.com>
2021-05-12 11:47:01 -0400

    [ADD] prop businessId for productForm for theme 2 and 3 (#552)
    Co-authored-by: Jesus Perez <jesus@ordering.co>

--- src/themes/two/src/components/Cart/index.js
+++ src/themes/two/src/components/Cart/index.js
@@ -253,7 +253,7 @@ const CartUI = (props) => {
           isCartProduct
           productCart={curProduct}
           businessSlug={cart?.business?.slug}
-          businessId={curProduct?.business_id}
+          businessId={cart?.business_id}
           categoryId={curProduct?.category_id}
           productId={curProduct?.id}
           onSave={handlerProductAction}
