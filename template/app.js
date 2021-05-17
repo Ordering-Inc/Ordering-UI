7c2edf9ca19c83f9d338ad566dd0428ff9a35486
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2021-05-17 19:51:14 -0400

    [FEAT] improve home loading (#556)

--- template/app.js
+++ template/app.js
@@ -38,7 +38,7 @@ export const App = () => {
   const [{ auth, user, loading }, { login }] = useSession()
   const [orderStatus] = useOrder()
   const [{ configs }] = useConfig()
-  const [, t] = useLanguage()
+  const [languageState, t] = useLanguage()
   const [loaded, setLoaded] = useState(false)
   const onlineStatus = useOnlineStatus()
   const location = useLocation()
@@ -98,12 +98,12 @@ export const App = () => {
       )}
       <ListenPageChanges />
       {
-        !loaded && (
+        (!loaded || languageState.loading) && (
           <SpinnerLoader />
         )
       }
       {
-        loaded && (
+        loaded && !languageState.loading && (
           <>
             <Header isHome={isHome} location={location} />
             <NotNetworkConnectivity />
