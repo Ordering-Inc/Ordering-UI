0062c255f40f835974e73e550b833088ef911d4b
Jesus Perez <jesus@ordering.co>
2021-05-18 15:03:43 -0400

    [ADD] version for sentry

--- index-template.js
+++ index-template.js
@@ -73,6 +73,7 @@ const configFile = {
 Sentry.init({
   environment: window?.location?.hostname === 'localhost' ? 'development' : process.env.NODE_ENV,
   dsn: 'https://ab508d2c6990411c8da375c997f9f3d6@o460529.ingest.sentry.io/5496646',
+  release: 'ordering-ui@' + process.env.npm_package_version,
   integrations: [
     new Integrations.BrowserTracing()
   ],
