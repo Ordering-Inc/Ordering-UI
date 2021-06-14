cd3b7ac0e20f746d5161dc29e40c7b800d5fb139
carlosq-ordering <carlos.q@ordering.co>
2021-06-14 12:38:51 -0400

    feat: show all order status

--- src/components/OrdersOption/index.js
+++ src/components/OrdersOption/index.js
@@ -209,7 +209,9 @@ export const OrdersOption = (props) => {
   const orderListProps = {
     ...props,
     UIComponent: OrdersOptionUI,
-    orderStatus: props.activeOrders ? [0, 3, 4, 7, 8, 9, 13, 14, 15, 18, 19, 20, 21] : [1, 2, 5, 6, 10, 11, 12, 16, 17],
+    orderStatus: props.activeOrders
+      ? [0, 3, 4, 7, 8, 9, 13, 14, 15, 18, 19, 20, 21]
+      : [1, 2, 5, 6, 10, 11, 12, 16, 17],
     useDefualtSessionManager: true,
     paginationSettings: {
       initialPage: 1,
