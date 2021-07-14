9bf82ba8daee21d8363041187621f2c6c2ad92fd
Min <skyandarti@outlook.com>
2021-07-14 23:19:20 +0800

    Home page completed

---
+++ src/themes/five/src/components/FacebookLogin/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+import { Button } from '../../styles/Buttons'
+
+export const FacebookButton = styled(Button)`
+  border: 1px solid #000000;
+  background: transparent;
+  border-radius: 7.6px;
+  font-size: 16px;
+  font-weight: 400;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 10px;
+
+  svg {
+    font-size: 16px;
+    color: #1977F2;
+  }
+`
