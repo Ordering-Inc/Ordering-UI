5e21ebf1ee02d828ea4792764580191f90fd1e9d
Min <skyandarti@outlook.com>
2021-07-26 18:34:28 +0800

    Update: index file

--- src/themes/five/index.js
+++ src/themes/five/index.js
@@ -1,5 +1,115 @@
+import { AddressesPopover } from './src/components/AddressesPopover'
+import { AddressForm } from './src/components/AddressForm'
+import { AddressList } from './src/components/AddressList'
+import { AppleLogin } from './src/components/AppleLogin'
+import { AutoScroll } from './src/components/AutoScroll'
+import { BusinessBasicInformation } from './src/components/BusinessBasicInformation'
+import { BusinessController } from './src/components/BusinessController'
+import { BusinessesListing } from './src/components/BusinessesListing'
+import { BusinessInformation } from './src/components/BusinessInformation'
+import { BusinessItemAccordion } from './src/components/BusinessItemAccordion'
+import { BusinessProductsCategories } from './src/components/BusinessProductsCategories'
+import { BusinessProductsList } from './src/components/BusinessProductsList'
+import { BusinessProductsListing } from './src/components/BusinessProductsListing'
+import { BusinessReviews } from './src/components/BusinessReviews'
+import { BusinessTypeFilter } from './src/components/BusinessTypeFilter'
+import { CardForm } from './src/components/CardForm'
+import { Cart } from './src/components/Cart'
+import { CartContent } from './src/components/CartContent'
+import { CartPopover } from './src/components/CartPopover'
+import { Checkout } from './src/components/Checkout'
+import { DriverTips } from './src/components/DriverTips'
+import { FacebookLogin } from './src/components/FacebookLogin'
+import { Footer } from './src/components/Footer'
+import { GoogleLogin } from './src/components/GoogleLogin'
+import { Header } from './src/components/Header'
+import { HeaderOption } from './src/components/HeaderOption'
 import { HomeHero } from './src/components/HomeHero'
+import { HorizontalOrdersLayout } from './src/components/HorizontalOrdersLayout'
+import { InputPhoneNumber } from './src/components/InputPhoneNumber'
+import { LoginForm } from './src/components/LoginForm'
+import { MomentContent } from './src/components/MomentContent'
+import { MomentControl } from './src/components/MomentControl'
+import { MomentPopover } from './src/components/MomentPopover'
+import { MyOrders } from './src/components/MyOrders'
+import { OrderDetails } from './src/components/OrderDetails'
+import { OrdersOption } from './src/components/OrdersOption'
+import { OrderTypeSelectorContent } from './src/components/OrderTypeSelectorContent'
+import { OrderTypeSelectorHeader } from './src/components/OrderTypeSelectorHeader'
+import { PaymentOptionCash } from './src/components/PaymentOptionCash'
+import { PaymentOptions } from './src/components/PaymentOptions'
+import { ProductForm } from './src/components/ProductForm'
+import { ProductItemAccordion } from './src/components/ProductItemAccordion'
+import { SearchBar } from './src/components/SearchBar'
+import { SidebarMenu } from './src/components/SidebarMenu'
+import { SignUpForm } from './src/components/SignUpForm'
+import { SingleProductCard } from './src/components/SingleProductCard'
+import { StripeElementsForm } from './src/components/StripeElementsForm'
+import { UpsellingPage } from './src/components/UpsellingPage'
+import { UserDetails } from './src/components/UserDetails'
+import { UserPopover } from './src/components/UserPopover'
+import { UserProfileForm } from './src/components/UserProfileForm'
+
+import { Button } from './src/styles/Buttons'
+import { Input, TextArea } from './src/styles/Inputs'
+import { Select } from './src/styles/Select'
 
 export {
-  HomeHero
+  AddressesPopover,
+  AddressForm,
+  AddressList,
+  AppleLogin,
+  AutoScroll,
+  BusinessBasicInformation,
+  BusinessController,
+  BusinessesListing,
+  BusinessInformation,
+  BusinessItemAccordion,
+  BusinessProductsCategories,
+  BusinessProductsList,
+  BusinessProductsListing,
+  BusinessReviews,
+  BusinessTypeFilter,
+  CardForm,
+  Cart,
+  CartContent,
+  CartPopover,
+  Checkout,
+  DriverTips,
+  FacebookLogin,
+  Footer,
+  GoogleLogin,
+  Header,
+  HeaderOption,
+  HomeHero,
+  HorizontalOrdersLayout,
+  InputPhoneNumber,
+  LoginForm,
+  MomentContent,
+  MomentControl,
+  MomentPopover,
+  MyOrders,
+  OrderDetails,
+  OrdersOption,
+  OrderTypeSelectorContent,
+  OrderTypeSelectorHeader,
+  PaymentOptionCash,
+  PaymentOptions,
+  ProductForm,
+  ProductItemAccordion,
+  SearchBar,
+  SidebarMenu,
+  SignUpForm,
+  SingleProductCard,
+  StripeElementsForm,
+  UpsellingPage,
+  UserDetails,
+  UserPopover,
+  UserProfileForm,
+
+  // styles
+  Button,
+  Input,
+  TextArea,
+  Select
 }
