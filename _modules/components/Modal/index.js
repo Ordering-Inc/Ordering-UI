e15c57cddc11670045d9aee34c11a95beb3c8d9f
carlosq-ordering <carlos.q@ordering.co>
2021-08-02 14:27:23 -0400

    build

--- _modules/components/Modal/index.js
+++ _modules/components/Modal/index.js
@@ -64,6 +64,12 @@ var ModalUI = function ModalUI(props) {
   };
 
   (0, _react.useEffect)(function () {
+    if (window.innerWidth > document.body.clientWidth) {
+      var scrollbarWidth = window.innerWidth - document.body.clientWidth;
+      var bodyPaddingRight = window.document.body.style.paddingRight;
+      document.body.style.paddingRight = props.open ? "".concat(bodyPaddingRight + scrollbarWidth, "px") : "".concat(bodyPaddingRight, "px");
+    }
+
     document.body.style.overflow = props.open ? 'hidden' : 'auto';
 
     if (props.open) {
