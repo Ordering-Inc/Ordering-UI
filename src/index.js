72a3b16a68151bb2df8d4e355f245db7dbc7124a
felix <felixmedina07052000@gmail.com>
2021-08-12 14:03:36 -0400

    fix:resolving conflict of syntax

--- src/index.js
+++ src/index.js
@@ -1,107 +1,93 @@
-import { AddressDetails } from "./components/AddressDetails";
-import { AddressesPopover } from "./components/AddressesPopover";
-import { AddressForm } from "./components/AddressForm";
-import { AddressList } from "./components/AddressList";
-import { AutoScroll } from "./components/AutoScroll";
-import { BusinessBasicInformation } from "./components/BusinessBasicInformation";
-import { BusinessController } from "./components/BusinessController";
-import { BusinessesListing } from "./components/BusinessesListing";
-import { BusinessesMap } from "./components/BusinessesMap";
-import { BusinessInformation } from "./components/BusinessInformation";
-import { BusinessItemAccordion } from "./components/BusinessItemAccordion";
-import { BusinessProductsCategories } from "./components/BusinessProductsCategories";
-import { BusinessProductsList } from "./components/BusinessProductsList";
-import { BusinessProductsListing } from "./components/BusinessProductsListing";
-import { BusinessReviews } from "./components/BusinessReviews";
-import { BusinessTypeFilter } from "./components/BusinessTypeFilter";
-import { CardForm } from "./components/CardForm";
-import { Cart } from "./components/Cart";
-import { CartContent } from "./components/CartContent";
-import { CartPopover } from "./components/CartPopover";
-import { Checkout } from "./components/Checkout";
-import { Cms } from "./components/Cms";
-import { Confirm, Alert } from "./components/Confirm";
-import { CouponControl } from "./components/CouponControl";
-import { DriverTips } from "./components/DriverTips";
-import {
-  Dropdown,
-  DropDownMenu,
-  DropdownItem,
-  DropdownToggle,
-} from "./components/Dropdown";
-import { FacebookLoginButton } from "./components/FacebookLogin";
-import { FloatingButton } from "./components/FloatingButton";
-import { Footer } from "./components/Footer";
-import { ForgotPasswordForm } from "./components/ForgotPasswordForm";
-import { GoogleGpsButton } from "./components/GoogleGpsButton";
-import { Header } from "./components/Header";
-import { HeaderOption } from "./components/HeaderOption";
-import { HomeHero } from "./components/HomeHero";
-import { HorizontalOrdersLayout } from "./components/HorizontalOrdersLayout";
-import { Image } from "./components/Image";
-import { InputPhoneNumber } from "./components/InputPhoneNumber";
-import { LanguageSelector } from "./components/LanguageSelector";
-import { LoginForm } from "./components/LoginForm";
-import { LogoutButton } from "./components/LogoutButton";
-import { Messages } from "./components/Messages";
-import { Modal } from "./components/Modal";
-import { MomentContent } from "./components/MomentContent";
-import { MomentControl } from "./components/MomentControl";
-import { MomentPopover } from "./components/MomentPopover";
-import { MyOrders } from "./components/MyOrders";
-import { NotFoundSource } from "./components/NotFoundSource";
-import { NotNetworkConnectivity } from "./components/NotNetworkConnectivity";
-import { OrderDetails } from "./components/OrderDetails";
-import { OrdersOption } from "./components/OrdersOption";
-import { OrderTypeSelectorHeader } from "./components/OrderTypeSelectorHeader";
-import { PageNotFound } from "./components/PageNotFound";
-import { PagesList } from "./components/PagesList";
-import { PaymentOptionCash } from "./components/PaymentOptionCash";
-import { PaymentOptionPaypal } from "./components/PaymentOptionPaypal";
-import { PaymentOptions } from "./components/PaymentOptions";
-import { PaymentOptionStripe } from "./components/PaymentOptionStripe";
-import { PhoneAutocomplete } from "./components/PhoneAutocomplete";
-import { ProductForm } from "./components/ProductForm";
-import { ProductIngredient } from "./components/ProductIngredient";
-import { ProductItemAccordion } from "./components/ProductItemAccordion";
-import { ProductOption } from "./components/ProductOption";
-import { ProductOptionSubOption } from "./components/ProductOptionSubOption";
-import { ProductShare } from "./components/ProductShare";
-import { ResetPassword } from "./components/ResetPassword";
-import { ReviewOrder } from "./components/ReviewOrder";
-import { SearchBar } from "./components/SearchBar";
-import { SidebarMenu } from "./components/SidebarMenu";
-import { SignUpForm } from "./components/SignUpForm";
-import { SingleProductCard } from "./components/SingleProductCard";
-import { SpinnerLoader } from "./components/SpinnerLoader";
-import { StripeElementsForm } from "./components/StripeElementsForm";
-import { StripeRedirectForm } from "./components/StripeRedirectForm";
-import { UpsellingPage } from "./components/UpsellingPage";
-import { UserDetails } from "./components/UserDetails";
-import { UserFormDetailsUI } from "./components/UserFormDetails";
-import { UserPopover } from "./components/UserPopover";
-import { UserProfileForm } from "./components/UserProfileForm";
-import { VerticalOrdersLayout } from "./components/VerticalOrdersLayout";
+import { AddressDetails } from './components/AddressDetails'
+import { AddressesPopover } from './components/AddressesPopover'
+import { AddressForm } from './components/AddressForm'
+import { AddressList } from './components/AddressList'
+import { AutoScroll } from './components/AutoScroll'
+import { BusinessBasicInformation } from './components/BusinessBasicInformation'
+import { BusinessController } from './components/BusinessController'
+import { BusinessesListing } from './components/BusinessesListing'
+import { BusinessesMap } from './components/BusinessesMap'
+import { BusinessInformation } from './components/BusinessInformation'
+import { BusinessItemAccordion } from './components/BusinessItemAccordion'
+import { BusinessProductsCategories } from './components/BusinessProductsCategories'
+import { BusinessProductsList } from './components/BusinessProductsList'
+import { BusinessProductsListing } from './components/BusinessProductsListing'
+import { BusinessReviews } from './components/BusinessReviews'
+import { BusinessTypeFilter } from './components/BusinessTypeFilter'
+import { CardForm } from './components/CardForm'
+import { Cart } from './components/Cart'
+import { CartContent } from './components/CartContent'
+import { CartPopover } from './components/CartPopover'
+import { Checkout } from './components/Checkout'
+import { Cms } from './components/Cms'
+import { Confirm, Alert } from './components/Confirm'
+import { CouponControl } from './components/CouponControl'
+import { DriverTips } from './components/DriverTips'
+import { Dropdown, DropDownMenu, DropdownItem, DropdownToggle } from './components/Dropdown'
+import { FacebookLoginButton } from './components/FacebookLogin'
+import { FloatingButton } from './components/FloatingButton'
+import { Footer } from './components/Footer'
+import { ForgotPasswordForm } from './components/ForgotPasswordForm'
+import { GoogleGpsButton } from './components/GoogleGpsButton'
+import { Header } from './components/Header'
+import { HeaderOption } from './components/HeaderOption'
+import { HomeHero } from './components/HomeHero'
+import { HorizontalOrdersLayout } from './components/HorizontalOrdersLayout'
+import { Image } from './components/Image'
+import { InputPhoneNumber } from './components/InputPhoneNumber'
+import { LanguageSelector } from './components/LanguageSelector'
+import { LoginForm } from './components/LoginForm'
+import { LogoutButton } from './components/LogoutButton'
+import { Messages } from './components/Messages'
+import { Modal } from './components/Modal'
+import { MomentContent } from './components/MomentContent'
+import { MomentControl } from './components/MomentControl'
+import { MomentPopover } from './components/MomentPopover'
+import { MyOrders } from './components/MyOrders'
+import { NotFoundSource } from './components/NotFoundSource'
+import { NotNetworkConnectivity } from './components/NotNetworkConnectivity'
+import { OrderDetails } from './components/OrderDetails'
+import { OrdersOption } from './components/OrdersOption'
+import { OrderTypeSelectorHeader } from './components/OrderTypeSelectorHeader'
+import { PageNotFound } from './components/PageNotFound'
+import { PagesList } from './components/PagesList'
+import { PaymentOptionCash } from './components/PaymentOptionCash'
+import { PaymentOptionPaypal } from './components/PaymentOptionPaypal'
+import { PaymentOptions } from './components/PaymentOptions'
+import { PaymentOptionStripe } from './components/PaymentOptionStripe'
+import { PhoneAutocomplete } from './components/PhoneAutocomplete'
+import { ProductForm } from './components/ProductForm'
+import { ProductIngredient } from './components/ProductIngredient'
+import { ProductItemAccordion } from './components/ProductItemAccordion'
+import { ProductOption } from './components/ProductOption'
+import { ProductOptionSubOption } from './components/ProductOptionSubOption'
+import { ProductShare } from './components/ProductShare'
+import { ResetPassword } from './components/ResetPassword'
+import { ReviewOrder } from './components/ReviewOrder'
+import { SearchBar } from './components/SearchBar'
+import { SidebarMenu } from './components/SidebarMenu'
+import { SignUpForm } from './components/SignUpForm'
+import { SingleProductCard } from './components/SingleProductCard'
+import { SpinnerLoader } from './components/SpinnerLoader'
+import { StripeElementsForm } from './components/StripeElementsForm'
+import { StripeRedirectForm } from './components/StripeRedirectForm'
+import { UpsellingPage } from './components/UpsellingPage'
+import { UserDetails } from './components/UserDetails'
+import { UserFormDetailsUI } from './components/UserFormDetails'
+import { UserPopover } from './components/UserPopover'
+import { UserProfileForm } from './components/UserProfileForm'
+import { VerticalOrdersLayout } from './components/VerticalOrdersLayout'
 
-import { ThemeContext, ThemeProvider, useTheme } from "./contexts/ThemeContext";
-import { useOnlineStatus } from "./hooks/useOnlineStatus";
-import { useWindowSize } from "./hooks/useWindowSize";
-import { useCountdownTimer } from "./hooks/useCountdownTimer";
+import { ThemeContext, ThemeProvider, useTheme } from './contexts/ThemeContext'
+import { useOnlineStatus } from './hooks/useOnlineStatus'
+import { useWindowSize } from './hooks/useWindowSize'
+import { useCountdownTimer } from './hooks/useCountdownTimer'
 
-import { Button } from "./styles/Buttons";
-import {
-  Input,
-  InputGroup,
-  InputPrimary,
-  InputGroupLeft,
-  InputGroupPrimary,
-  InputGroupRight,
-  InputSecundary,
-  TextArea,
-} from "./styles/Inputs";
-import { Select } from "./styles/Select";
-import { Tab, Tabs } from "./styles/Tabs";
-import { Toast } from "./styles/Toast";
+import { Button } from './styles/Buttons'
+import { Input, InputGroup, InputPrimary, InputGroupLeft, InputGroupPrimary, InputGroupRight, InputSecundary, TextArea } from './styles/Inputs'
+import { Select } from './styles/Select'
+import { Tab, Tabs } from './styles/Tabs'
+import { Toast } from './styles/Toast'
 // additional
 import {
   sortInputFields,
@@ -110,8 +96,8 @@ import {
   scrollTo,
   DriverTipsOptions,
   bytesConverter,
-  formatSeconds,
-} from "./utils";
+  formatSeconds
+} from './utils'
 
 export {
   AddressDetails,
@@ -226,5 +212,5 @@ export {
   scrollTo,
   DriverTipsOptions,
   bytesConverter,
-  formatSeconds,
-};
+  formatSeconds
+}
