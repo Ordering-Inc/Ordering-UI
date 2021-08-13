16306d75c5c3d0eb5a10a572a0a63d67c26f59c2
carlosg-ordering <68757729+carlosg-ordering@users.noreply.github.com>
2021-08-13 11:16:07 -0500

    Add imports for template 6 (#672)

--- src/themes/six/index.js
+++ src/themes/six/index.js
@@ -46,11 +46,16 @@ import { SingleProductCard } from './src/components/SingleProductCard'
 import { UpsellingPage } from './src/components/UpsellingPage'
 import { UserDetails } from './src/components/UserDetails'
 import { UserFormDetails } from './src/components/UserFormDetails'
+import { NotFoundSource } from './src/components/NotFoundSource'
+import { ForgotPasswordForm } from './src/components/ForgotPasswordForm'
+import { ProductIngredient } from './src/components/ProductIngredient'
 
 import { Inputs } from './src/styles/Inputs'
 import { Select } from './src/styles/Select'
 import { Selects } from './src/styles/Selects'
 import { Tabs } from './src/styles/Tabs'
+import { Button } from '../../styles/Buttons'
+import { TextArea } from '../../styles/Inputs'
 
 export {
   AddressDetails,
@@ -101,10 +106,15 @@ export {
   UpsellingPage,
   UserDetails,
   UserFormDetails,
-
+  NotFoundSource,
+  ForgotPasswordForm,
+  ProductIngredient,
+  
   // styles
   Inputs,
   Select,
   Selects,
-  Tabs
+  Tabs,
+  Button,
+  TextArea
 }
