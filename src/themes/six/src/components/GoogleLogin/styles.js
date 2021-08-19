4226069c0878152120d3acb7d32dbf8581cd3198
Felix Medina <50598170+felixmedina07@users.noreply.github.com>
2021-08-19 15:32:11 -0400

    fix:change implements components in theme six (#680)

---
+++ src/themes/six/src/components/GoogleLogin/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+import { Button } from '../../styles/Buttons'
+
+export const GoogleButton = styled(Button)`
+  background-color: #EFEFEF;
+  border-color: #EFEFEF;
+  font-size: 16px;
+  font-weight: 400;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 10px;
+  svg {
+    font-size: 28px;
+    color: #1977F2;
+  }
+
+  div {
+    margin: 0 auto;
+  }
+`
