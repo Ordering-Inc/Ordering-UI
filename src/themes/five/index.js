fc018676473f75bb0cd31eb4ab871daaf97fff50
Min <skyandarti@outlook.com>
2021-09-09 03:11:54 +0800

    Feat: help on theme_five

--- src/themes/five/index.js
+++ src/themes/five/index.js
@@ -26,9 +26,11 @@ import { GoogleLogin } from './src/components/GoogleLogin'
 import { Header } from './src/components/Header'
 import { HeaderOption } from './src/components/HeaderOption'
 import { Help } from './src/components/Help'
+import { HelpStaticPage } from './src/components/HelpStaticPage'
 import { HomeHero } from './src/components/HomeHero'
 import { HorizontalOrdersLayout } from './src/components/HorizontalOrdersLayout'
 import { InputPhoneNumber } from './src/components/InputPhoneNumber'
+import { LastOrders } from './src/components/LastOrders'
 import { LoginForm } from './src/components/LoginForm'
 import { Messages } from './src/components/Messages'
 import { MomentContent } from './src/components/MomentContent'
@@ -88,9 +90,11 @@ export {
   Header,
   HeaderOption,
   Help,
+  HelpStaticPage,
   HomeHero,
   HorizontalOrdersLayout,
   InputPhoneNumber,
+  LastOrders,
   LoginForm,
   Messages,
   MomentContent,
