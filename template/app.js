ec46678dfa16b8ef59803135577125652905c135
Min <skyandarti@outlook.com>
2021-09-09 03:37:27 +0800

    Update: project build

--- template/app.js
+++ template/app.js
@@ -8,8 +8,6 @@ import {
 } from 'react-router-dom'
 import { useSession, useLanguage, useOrder, Analytics, useConfig } from 'ordering-components'
 
-// import { Header } from '../src/themes/five/src/components/Header'
-// import { Footer } from '../src/themes/two/src/components/Footer'
 import { Header } from '../src/components/Header'
 import { Footer } from '../src/components/Footer'
 import { SpinnerLoader } from '../src/components/SpinnerLoader'
