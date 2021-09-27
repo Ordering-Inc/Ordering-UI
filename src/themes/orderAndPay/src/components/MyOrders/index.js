74da04e51ecc51969e029ed3dd1a4d322e869fbe
Jesus David <50583541+YisusJe@users.noreply.github.com>
2021-09-27 13:14:37 -0400

    fixes 4 (#727)

---
+++ src/themes/orderAndPay/src/components/MyOrders/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Container } from './styles'
+
+import { OrdersOption } from '../OrdersOption'
+
+export const MyOrders = (props) => {
+  return (
+    <>
+      {props.beforeElements?.map((BeforeElement, i) => (
+        <React.Fragment key={i}>
+          {BeforeElement}
+        </React.Fragment>))}
+      {props.beforeComponents?.map((BeforeComponent, i) => (
+        <BeforeComponent key={i} {...props} />))}
+      <Container>
+        <OrdersOption {...props} activeOrders />
+      </Container>
+      {props.afterComponents?.map((AfterComponent, i) => (
+        <AfterComponent key={i} {...props} />))}
+      {props.afterElements?.map((AfterElement, i) => (
+        <React.Fragment key={i}>
+          {AfterElement}
+        </React.Fragment>))}
+    </>
+  )
+}
