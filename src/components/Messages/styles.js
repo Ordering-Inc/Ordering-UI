3cb7723a994e023be592f584275abe5f24ffde0b
Min <skyandarti@outlook.com>
2021-10-06 04:19:50 +0800

    Update: disable for driver chat

--- src/components/Messages/styles.js
+++ src/components/Messages/styles.js
@@ -469,3 +469,23 @@ export const ModalIcon = styled.span`
   `}
   }
 `
+
+export const NotSendMessage = styled(SendForm)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  color: #909BA9;
+  background: #FAFAFA;
+
+  svg {
+    font-size: 20px;
+  }
+
+  p {
+    font-size: 14px;
+    color: #909BA9;
+    margin: 6px 0;
+  }
+`
