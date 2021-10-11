e12ef4ecf1d9ec66cd24af63563d4eb3e54928a9
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2021-10-12 00:43:34 +0800

    build

--- _modules/themes/eight/src/components/SingleProductCard/styles.js
+++ _modules/themes/eight/src/components/SingleProductCard/styles.js
@@ -25,13 +25,10 @@ function _extends() { _extends = Object.assign || function (target) { for (var i
 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-var CardContainer = _styledComponents.default.div(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n  display: flex;\n  flex-direction: column;\n  width: calc(100% - 40px);\n  cursor: pointer;\n  position: relative;\n  background: ", ";\n  margin: 10px;\n  padding: 20px;\n  box-sizing: border-box;\n  border-radius: 7.6px;\n\n  &:hover {\n    box-shadow: rgb(0 0 0 / 20%) 0px 2px 4px;\n    background-color: ", ";\n  }\n\n  ", "\n"])), function (_ref) {
-  var soldOut = _ref.soldOut;
-  return soldOut ? '#B8B8B8' : '#FFF';
-}, function (props) {
+var CardContainer = _styledComponents.default.div(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n  display: flex;\n  flex-direction: column;\n  width: calc(100% - 40px);\n  cursor: pointer;\n  position: relative;\n  margin: 10px;\n  padding: 20px;\n  box-sizing: border-box;\n  border-radius: 7.6px;\n\n  &:hover {\n    box-shadow: rgb(0 0 0 / 20%) 0px 2px 4px;\n    background-color: ", ";\n  }\n\n  ", "\n"])), function (props) {
   return props.theme.colors.third;
-}, function (_ref2) {
-  var isCartOnProductsList = _ref2.isCartOnProductsList;
+}, function (_ref) {
+  var isCartOnProductsList = _ref.isCartOnProductsList;
   return isCartOnProductsList ? (0, _styledComponents.css)(_templateObject2 || (_templateObject2 = _taggedTemplateLiteral(["\n    @media (min-width: 993px) {\n      width: calc(50% - 20px);\n    }\n  "]))) : (0, _styledComponents.css)(_templateObject3 || (_templateObject3 = _taggedTemplateLiteral(["\n    @media (min-width: 681px) {\n      width: calc(50% - 20px);\n    }\n\n    @media (min-width: 993px) {\n      width: calc(33% - 20px);\n    }\n  "])));
 });
 
@@ -45,21 +42,21 @@ var SoldOut = _styledComponents.default.span(_templateObject4 || (_templateObjec
 
 exports.SoldOut = SoldOut;
 
-var CardInfo = _styledComponents.default.div(_templateObject6 || (_templateObject6 = _taggedTemplateLiteral(["\n  display: flex;\n  flex-direction: column;\n  h1 {\n    font-size: 16px;\n    font-weight: ", ";\n    text-align: left;\n    overflow: hidden;\n    text-overflow: ellipsis;\n    white-space: nowrap;\n    text-align: center;\n    color: ", ";\n    margin: 0px;\n    padding-top: 10px;\n  }\n\n  p {\n    font-weight: ", ";\n    text-align: left;\n    ", "\n\n    display: -webkit-box;\n    -webkit-box-orient: vertical;\n    -webkit-line-clamp: 2;\n    overflow: hidden;\n  }\n\n  span {\n    color: ", ";\n    font-weight: bold;\n  }\n\n  p, span {\n    font-size: 13px;\n  }\n\n  @media (min-width: 1024px) {\n    p {\n      font-size: 16px;\n    }\n\n    span {\n      font-size: 15px;\n    }\n  }\n"])), function (_ref3) {
-  var soldOut = _ref3.soldOut;
+var CardInfo = _styledComponents.default.div(_templateObject6 || (_templateObject6 = _taggedTemplateLiteral(["\n  display: flex;\n  flex-direction: column;\n  h1 {\n    font-size: 16px;\n    font-weight: ", ";\n    text-align: left;\n    overflow: hidden;\n    text-overflow: ellipsis;\n    white-space: nowrap;\n    text-align: center;\n    color: ", ";\n    margin: 0px;\n    padding-top: 10px;\n  }\n\n  p {\n    font-weight: ", ";\n    text-align: left;\n    ", "\n\n    display: -webkit-box;\n    -webkit-box-orient: vertical;\n    -webkit-line-clamp: 2;\n    overflow: hidden;\n  }\n\n  span {\n    color: ", ";\n    font-weight: bold;\n  }\n\n  p, span {\n    font-size: 13px;\n  }\n\n  @media (min-width: 1024px) {\n    p {\n      font-size: 16px;\n    }\n\n    span {\n      font-size: 15px;\n    }\n  }\n"])), function (_ref2) {
+  var soldOut = _ref2.soldOut;
   return soldOut ? 'bold' : '500';
 }, function (props) {
   return props.theme.colors.secundary;
-}, function (_ref4) {
-  var soldOut = _ref4.soldOut;
+}, function (_ref3) {
+  var soldOut = _ref3.soldOut;
   return soldOut ? 'bold' : '200';
 }, function (props) {
   var _props$theme2;
 
   return ((_props$theme2 = props.theme) === null || _props$theme2 === void 0 ? void 0 : _props$theme2.rtl) && (0, _styledComponents.css)(_templateObject7 || (_templateObject7 = _taggedTemplateLiteral(["\n      text-align: right;\n    "])));
-}, function (_ref5) {
-  var soldOut = _ref5.soldOut,
-      theme = _ref5.theme;
+}, function (_ref4) {
+  var soldOut = _ref4.soldOut,
+      theme = _ref4.theme;
   return soldOut ? (0, _polished.lighten)(0.05, theme.colors.primary) : theme.colors.primary;
 });
 
@@ -89,11 +86,11 @@ var CardLogo = function CardLogo(props) {
 
 exports.CardLogo = CardLogo;
 
-var QuantityContainer = _styledComponents.default.div(_templateObject10 || (_templateObject10 = _taggedTemplateLiteral(["\n  background: ", ";\n  color: ", ";\n  position: absolute;\n  left: 100%;\n  bottom: 100%;\n  width: 25px;\n  height: 25px;\n  text-align: center;\n  border-radius: 50%;\n  transform: translate(-50%, 50%);\n"])), function (_ref6) {
-  var theme = _ref6.theme;
+var QuantityContainer = _styledComponents.default.div(_templateObject10 || (_templateObject10 = _taggedTemplateLiteral(["\n  background: ", ";\n  color: ", ";\n  position: absolute;\n  left: 100%;\n  bottom: 100%;\n  width: 25px;\n  height: 25px;\n  text-align: center;\n  border-radius: 50%;\n  transform: translate(-33px,33px);\n  z-index: 1;\n"])), function (_ref5) {
+  var theme = _ref5.theme;
   return theme.colors.primary;
-}, function (_ref7) {
-  var theme = _ref7.theme;
+}, function (_ref6) {
+  var theme = _ref6.theme;
   return theme.colors.primaryContrast;
 });
 
