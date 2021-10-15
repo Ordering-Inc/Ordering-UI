151b2e5e26c82fe05ffae8a7789f072a8ad1c35a
Min <skyandarti@outlook.com>
2021-10-15 18:26:55 +0800

    Update: remove build files

--- template/components/ListenPageChanges/index.js
+++ template/components/ListenPageChanges/index.js
@@ -20,7 +20,6 @@ export const ListenPageChanges = ({ children }) => {
     signup: '/signup',
     signin: '/signin',
     profile: '/profile',
-    saved_places: '/saved_places',
     orders: '/profile/orders',
     order_detail: '/orders/:orderId',
     checkout: '/checkout/:cartUuid',
