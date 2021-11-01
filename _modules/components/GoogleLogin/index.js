aeb004b3e0e32b64cea3194e528d4d644f84d5c0
carlosq-ordering <carlos.q@ordering.co>
2021-11-01 18:48:59 -0400

    build

--- _modules/components/GoogleLogin/index.js
+++ _modules/components/GoogleLogin/index.js
@@ -9,15 +9,13 @@ var _react = _interopRequireDefault(require("react"));
 
 var _orderingComponents = require("ordering-components");
 
-var _test = require("./test");
-
 var _FcGoogle = _interopRequireDefault(require("@meronex/icons/fc/FcGoogle"));
 
 var _styles = require("./styles");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
@@ -31,7 +29,7 @@ function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o =
 
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
-function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
@@ -40,21 +38,12 @@ var GoogleLoginButtonUI = function GoogleLoginButtonUI(props) {
       _useLanguage2 = _slicedToArray(_useLanguage, 2),
       t = _useLanguage2[1];
 
-  var signIn = props.signIn,
-      initParams = props.initParams;
-  return /*#__PURE__*/_react.default.createElement(_styles.GoogleButton, null, /*#__PURE__*/_react.default.createElement("div", {
-    id: "g_id_onload",
-    "data-client_id": "813628525689-pjdt5ufp3tot6jc1tkn6vecbvl928qde.apps.googleusercontent.com",
-    "data-auto_prompt": "false"
-  }), /*#__PURE__*/_react.default.createElement("div", {
-    className: "g_id_signin",
-    "data-type": "standard",
-    "data-size": "large",
-    "data-theme": "outline",
-    "data-text": "sign_in_with",
-    "data-shape": "rectangular",
-    "data-logo_alignment": "left"
-  }));
+  var signIn = props.signIn;
+  return /*#__PURE__*/_react.default.createElement(_styles.GoogleButton, {
+    initialIcon: true,
+    color: "secondary",
+    onClick: signIn
+  }, /*#__PURE__*/_react.default.createElement(_FcGoogle.default, null), /*#__PURE__*/_react.default.createElement("div", null, t('LOGIN_WITH_GOOGLE', 'Login with Google')));
 };
 
 exports.GoogleLoginButtonUI = GoogleLoginButtonUI;
@@ -64,7 +53,7 @@ var GoogleLoginButton = function GoogleLoginButton(props) {
     UIComponent: GoogleLoginButtonUI
   });
 
-  return /*#__PURE__*/_react.default.createElement(_test.GoogleLoginButton, googleLoginProps);
+  return /*#__PURE__*/_react.default.createElement(_orderingComponents.GoogleLoginButton, googleLoginProps);
 };
 
 exports.GoogleLoginButton = GoogleLoginButton;
--- _modules/themes/eight/src/components/Checkout/styles.js
+++ _modules/themes/eight/src/components/Checkout/styles.js
@@ -11,9 +11,9 @@ var _styledComponents = _interopRequireWildcard(require("styled-components"));
 
 var _templateObject, _templateObject2, _templateObject3, _templateObject4, _templateObject5, _templateObject6, _templateObject7, _templateObject8, _templateObject9, _templateObject10, _templateObject11, _templateObject12, _templateObject13, _templateObject14, _templateObject15, _templateObject16, _templateObject17, _templateObject18, _templateObject19, _templateObject20, _templateObject21, _templateObject22;
 
-function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
 
-function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
@@ -56,7 +56,7 @@ exports.WrapperUserDetails = WrapperUserDetails;
 var UserDetailsContainer = _styledComponents.default.div(_templateObject11 || (_templateObject11 = _taggedTemplateLiteral(["\n  display: flex;\n  justify-content: space-between;\n  margin: 30px 0px 20px;\n  flex-wrap: wrap;\n"])));
 
 exports.UserDetailsContainer = UserDetailsContainer;
-var BusinessDetailsContainer = (0, _styledComponents.default)(UserDetailsContainer)(_templateObject12 || (_templateObject12 = _taggedTemplateLiteral(["\n  div {\n    h1 {\n      text-transform: capitalize;\n      margin: 0px;\n      font-size: 20px;\n      color: ", ";\n      margin-bottom: 10px;\n      font-weight: 500;\n    }\n    p {\n      margin: 0;\n      color: ", ";\n    }\n  }\n"])), function (props) {
+var BusinessDetailsContainer = (0, _styledComponents.default)(UserDetailsContainer)(_templateObject12 || (_templateObject12 = _taggedTemplateLiteral(["\n  div {\n    h1 {\n      text-transform: capitalize;\n      margin: 0px;\n      font-size: 20px;\n      color: ", ";\n      margin-bottom: 10px;\n      font-weight: 500;\n    }\n    p {\n      margin: 0;\n      font-weight: 300;\n      color: ", ";\n    }\n  }\n"])), function (props) {
   return props.theme.colors.grayDark;
 }, function (props) {
   return props.theme.colors.grayDark;
