fe61bd237b2582962f580fb21934c48d6c535504
Jesus David <50583541+YisusJe@users.noreply.github.com>
2021-11-03 15:15:22 -0400

    Google identity (#782)
    * [add] google identity for login
    
    * [ADD] google identity
    
    * delete console logs
    
    Co-authored-by: carlosq-ordering <carlos.q@ordering.co>

---
+++ src/components/GoogleIdentity/index.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react'
+import { GoogleIdentityButton as GoogleIdentityButtonController } from 'ordering-components'
+import { GoogleButton } from './styles'
+
+export const GoogleLoginButtonUI = (props) => {
+  const { initParams, handleSigninSuccess } = props
+
+  useEffect(() => {
+    window.handleCredentialResponse = (response) => {
+      handleSigninSuccess(response.credential)
+    }
+    const GoogleButton = document.getElementById('g_id_onload')
+    GoogleButton.setAttribute('data-callback', 'handleCredentialResponse')
+  })
+
+  return (
+    <GoogleButton
+      initialIcon
+      color='secondary'
+      id='g_id_onload'
+      data-client_id={initParams.client_id}
+      data-auto_prompt='false'
+    >
+      <div
+        className='g_id_signin'
+        data-size='medium'
+        data-shape='pill'
+        data-text='signin_with'
+        data-locale='en'
+      />
+    </GoogleButton>
+  )
+}
+
+export const GoogleIdentityButton = (props) => {
+  const googleLoginProps = {
+    ...props,
+    UIComponent: GoogleLoginButtonUI
+  }
+  return <GoogleIdentityButtonController {...googleLoginProps} />
+}
