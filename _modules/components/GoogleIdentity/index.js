9f19c94d63b0de947c5690dd873526a502d06096
Carlos Gomez <carlos.g@ordering.co>
2021-11-16 16:41:59 -0600

    build

--- _modules/components/GoogleIdentity/index.js
+++ _modules/components/GoogleIdentity/index.js
@@ -13,6 +13,8 @@ var _orderingComponents = require("ordering-components");
 
 var _styles = require("./styles");
 
+var _Confirm = require("../Confirm");
+
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
@@ -23,9 +25,35 @@ function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { va
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 var GoogleLoginButtonUI = function GoogleLoginButtonUI(props) {
   var initParams = props.initParams,
-      handleSigninSuccess = props.handleSigninSuccess;
+      handleSigninSuccess = props.handleSigninSuccess,
+      formState = props.formState;
+
+  var _useLanguage = (0, _orderingComponents.useLanguage)(),
+      _useLanguage2 = _slicedToArray(_useLanguage, 2),
+      t = _useLanguage2[1];
+
+  var _useState = (0, _react.useState)({
+    open: false,
+    content: []
+  }),
+      _useState2 = _slicedToArray(_useState, 2),
+      alertState = _useState2[0],
+      setAlertState = _useState2[1];
+
   (0, _react.useEffect)(function () {
     window.handleCredentialResponse = function (response) {
       handleSigninSuccess(response.credential);
@@ -34,7 +62,15 @@ var GoogleLoginButtonUI = function GoogleLoginButtonUI(props) {
     var GoogleButton = document.getElementById('g_id_onload');
     GoogleButton.setAttribute('data-callback', 'handleCredentialResponse');
   });
-  return /*#__PURE__*/_react.default.createElement(_styles.GoogleButton, {
+  (0, _react.useEffect)(function () {
+    if (formState.error) {
+      setAlertState({
+        open: true,
+        content: formState.result
+      });
+    }
+  }, [formState]);
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement(_styles.GoogleButton, {
     initialIcon: true,
     color: "secondary",
     id: "g_id_onload",
@@ -46,6 +82,22 @@ var GoogleLoginButtonUI = function GoogleLoginButtonUI(props) {
     "data-shape": "pill",
     "data-text": "signin_with",
     "data-locale": "en"
+  })), /*#__PURE__*/_react.default.createElement(_Confirm.Alert, {
+    open: alertState.open,
+    title: t('GOOGLE_LOGIN_ERROR', 'Google Login error'),
+    content: alertState === null || alertState === void 0 ? void 0 : alertState.result,
+    onClose: function onClose() {
+      return setAlertState({
+        open: false,
+        content: []
+      });
+    },
+    onAccept: function onAccept() {
+      return setAlertState({
+        open: false,
+        content: []
+      });
+    }
   }));
 };
 
