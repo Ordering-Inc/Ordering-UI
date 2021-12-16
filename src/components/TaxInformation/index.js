eeaeebc139792b60ab6e350595ffeb00e7cf34f8
Jesus David <50583541+YisusJe@users.noreply.github.com>
2021-12-16 14:02:07 -0400

    [add] fallback for ancient orders with no taxes or fees (#840)

--- src/components/TaxInformation/index.js
+++ src/components/TaxInformation/index.js
@@ -22,7 +22,7 @@ export const TaxInformation = (props) => {
           {t('DESCRIPTION', 'Description')}: {data?.description} {data?.type && includedOnPriceString}
         </h2>
       )}
-      <h3>{t(`OTHER_PRODUCTS_WITH_SAME_${TaxFeeString.toUpperCase()}`, `Other products with the same ${TaxFeeString}`)}:</h3>
+      <h3>{t(`OTHER_PRODUCTS_WITH_THIS_${TaxFeeString.toUpperCase()}`, `Other products with this ${TaxFeeString}`)}:</h3>
       <ProductContainer>
         {
           products.filter(product => isTax ? (product.tax?.id ? product.tax?.id === data?.id : product.tax?.id === null && data?.id === null) : (product.fee?.id ? product.fee?.id === data?.id : (product.fee?.id === null && data?.id === null))).map(product => (
