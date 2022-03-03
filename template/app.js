c2db089dd171cc74eee363ebcb73c2502a1d84d2
Min <skyandarti@outlook.com>
2022-03-04 07:16:28 +0800

    Update

--- template/app.js
+++ template/app.js
@@ -8,9 +8,8 @@ import {
 } from 'react-router-dom'
 import { useSession, useLanguage, useOrder, Analytics, useConfig, FacebookPixel } from 'ordering-components'
 
-// import { Header } from '../src/components/Header'
-// import { Footer } from '../src/components/Footer'
-import { Header, Footer } from '../src/themes/five'
+import { Header } from '../src/components/Header'
+import { Footer } from '../src/components/Footer'
 import { SpinnerLoader } from '../src/components/SpinnerLoader'
 import { NotNetworkConnectivity } from '../src/components/NotNetworkConnectivity'
 import { useOnlineStatus } from '../src/hooks/useOnlineStatus'
@@ -97,23 +96,23 @@ export const App = () => {
 
   return (
     <>
-      {/* {!!configs?.track_id_google_analytics?.value && (
+      {!!configs?.track_id_google_analytics?.value && (
         <Analytics trackId={configs?.track_id_google_analytics?.value} />
       )}
       {!!configs?.facebook_id?.value && (
         <FacebookPixel trackId={configs?.facebook_id?.value} />
-      )} */}
+      )}
       <ListenPageChanges />
       {
         !loaded && (
           <SpinnerLoader />
         )
       }
-      {/* <SmartAppBanner
+      <SmartAppBanner
         storeAndroidId={settings?.store_android_id}
         storeAppleId={settings?.store_apple_id}
         storeKindleId={settings?.store_kindle_id}
-      /> */}
+      />
       {
         loaded && (
           <>
