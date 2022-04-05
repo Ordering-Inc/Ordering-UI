5bf3e250d67b979ffeb1cdbc92e6e8cb91a1b499
carlosg-ordering <68757729+carlosg-ordering@users.noreply.github.com>
2022-04-05 15:45:44 -0500

    [FIX] allow footer as cms (#993)

--- src/themes/five/src/components/Footer/index.js
+++ src/themes/five/src/components/Footer/index.js
@@ -1,89 +1,49 @@
-import React from 'react'
-import { useLanguage } from 'ordering-components'
-import FaFacebookF from '@meronex/icons/fa/FaFacebookF'
-import { Twitter, Instagram, Globe } from 'react-bootstrap-icons'
-import { LanguageSelector } from '../LanguageSelector'
-
-import {
-  Container,
-  FooterWrapper,
-  FooterBottom,
-  Content,
-  Description,
-  SocialIconList,
-  LanguageWrapper,
-  SocialGroup
-} from './styles'
+import React, { useState, useEffect } from 'react'
+import { useApi } from 'ordering-components'
 
 export const Footer = () => {
-  const [, t] = useLanguage()
+  const [footerState, setfooterState] = useState({ body: null, loading: false, error: null })
+  const [ordering] = useApi()
+  const requestsState = {}
+
+  const getPage = async () => {
+    setfooterState({ ...footerState, loading: true })
+    try {
+      const source = {}
+      requestsState.page = source
+      const { content: { error, result } } = await ordering.pages('footer').get({ cancelToken: source })
+      setfooterState({ ...footerState, loading: false })
+      if (!error) {
+        setfooterState({ ...footerState, body: result.body })
+      } else {
+        setfooterState({ ...footerState, error: result })
+      }
+    } catch (err) {
+      if (err.constructor.name !== 'Cancel') {
+        setfooterState({ ...footerState, loading: false, error: [err.message] })
+      }
+    }
+  }
+
+  useEffect(() => {
+    getPage()
+    return () => {
+      if (requestsState.page) {
+        requestsState.page.cancel()
+      }
+    }
+  }, [])
 
   return (
-    <Container>
-      <FooterWrapper>
-        <Content>
-          <h4>{t('FOOTER_QL1_TITLE')}</h4>
-          <div>
-            <a rel='noopener noreferrer' target='_blank' href={t('FOOTER_QL1_1_HREF')}>{t('FOOTER_QL1_1')}</a>
-          </div>
-          <div>
-            <a rel='noopener noreferrer' target='_blank' href={t('FOOTER_QL1_2_HREF')}>{t('FOOTER_QL1_2')}</a>
-          </div>
-          <div>
-            <a rel='noopener noreferrer' target='_blank' href={t('FOOTER_QL1_3_HREF')}>{t('FOOTER_QL1_3')}</a>
-          </div>
-        </Content>
-        <Content>
-          <h4>{t('FOOTER_QL2_TITLE')}</h4>
-          <div>
-            <a rel='noopener noreferrer' target='_blank' href={t('FOOTER_QL2_1_HREF')}>{t('FOOTER_QL2_1')}</a>
-          </div>
-          <div>
-            <a rel='noopener noreferrer' target='_blank' href={t('FOOTER_QL2_2_HREF')}>{t('FOOTER_QL2_2')}</a>
-          </div>
-          <div>
-            <a rel='noopener noreferrer' target='_blank' href={t('FOOTER_QL2_3_HREF')}>{t('FOOTER_QL2_3')}</a>
-          </div>
-        </Content>
-        <Content>
-          <h4>{t('FOOTER_QL3_TITLE')}</h4>
-          <div>
-            <a rel='noopener noreferrer' target='_blank' href={t('FOOTER_QL3_1_HREF')}>{t('FOOTER_QL3_1')}</a>
-          </div>
-          <div>
-            <a rel='noopener noreferrer' target='_blank' href={t('FOOTER_QL3_2_HREF')}>{t('FOOTER_QL3_2')}</a>
-          </div>
-          <div>
-            <a rel='noopener noreferrer' target='_blank' href={t('FOOTER_QL3_3_HREF')}>{t('FOOTER_QL3_3')}</a>
-          </div>
-        </Content>
-        <Content>
-          <h4>{t('FOOTER_QL4_TITLE')}</h4>
-          <div>
-            <a rel='noopener noreferrer' target='_blank' href={t('FOOTER_QL4_1_HREF')}>{t('FOOTER_QL4_1')}</a>
-          </div>
-          <div>
-            <a rel='noopener noreferrer' target='_blank' href={t('FOOTER_QL4_2_HREF')}>{t('FOOTER_QL4_2')}</a>
-          </div>
-          <div>
-            <a rel='noopener noreferrer' target='_blank' href={t('FOOTER_QL4_3_HREF')}>{t('FOOTER_QL4_3')}</a>
-          </div>
-        </Content>
-      </FooterWrapper>
-      <FooterBottom>
-        <Description>{t('FOOTER_DESC', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.')}</Description>
-        <SocialIconList>
-          <LanguageWrapper>
-            <Globe />
-            <LanguageSelector />
-          </LanguageWrapper>
-          <SocialGroup>
-            <a href='https://www.facebook.com/orderingco/' target='_blank' rel='noopener noreferrer'><FaFacebookF /></a>
-            <a href='https://twitter.com' target='_blank' rel='noopener noreferrer'><Twitter /></a>
-            <a href='https://www.instagram.com/ordering.co/' target='_blank' rel='noopener noreferrer'><Instagram /></a>
-          </SocialGroup>
-        </SocialIconList>
-      </FooterBottom>
-    </Container>
+    <>
+      {
+        footerState.body && (
+          <div dangerouslySetInnerHTML={{
+            __html: footerState.body
+          }}
+          />
+        )
+      }
+    </>
   )
 }
