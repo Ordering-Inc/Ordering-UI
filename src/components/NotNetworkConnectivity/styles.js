5c5da3d5daa8cc35c73947256508fe0cadc4466f
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-04-08 23:58:25 +0800

    Revert "[Update] No internet connection UI"
    This reverts commit f82949a25b7442f3e55fa6dd50a147b9d3af0c6e.

--- src/components/NotNetworkConnectivity/styles.js
+++ src/components/NotNetworkConnectivity/styles.js
@@ -4,50 +4,21 @@ export const Container = styled.div`
   display: ${({ isOnline }) => isOnline ? 'none' : 'flex'};
   flex-direction: column;
   justify-content: center;
-  width: 90%;
-  margin: 60px auto;
+  align-items: center;
+  width: 100%;
+  margin: 150px auto 100px;
   box-sizing: border-box;
 
-  > h1 {
-    font-size: 24px;
-    line-height: 36px;
-    font-weight: 600;
-    margin: 0;
-  }
-
-  > p {
-    font-size: 14px;
-    line-height: 24px;
-    margin: 13px 0;
-  }
-
   @media (min-width: 768px) {
-    width: 80%;
-  }
-
-  @media (min-width: 1024px) {
-    width: 60%;
+    margin: 70px auto;
   }
 `
 
 export const Image = styled.div`
-  margin-top: 70px;
-  display: flex;
-  justify-content: center;
+  padding-left: 20px;
   img {
-    max-width: 300px;
     width: 100%;
     height: auto;
     aspect-ratio: attr(width) / attr(height)
-  }
-`
-
-export const ButtonWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  margin: 34px 0;
-  > button {
-    height: 44px;
-    border-radius: 8px;
   }
 `
