f82949a25b7442f3e55fa6dd50a147b9d3af0c6e
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-04-08 23:30:24 +0800

    [Update] No internet connection UI

--- src/components/NotNetworkConnectivity/index.js
+++ src/components/NotNetworkConnectivity/index.js
@@ -1,39 +1,53 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useLanguage } from 'ordering-components'
 import { useTheme } from 'styled-components'
 import { useOnlineStatus } from '../../hooks/useOnlineStatus'
+import { Button } from '../../styles/Buttons'
 
 import {
   Container,
-  Image
+  Image,
+  ButtonWrapper
 } from './styles'
 
 export const NotNetworkConnectivity = (props) => {
+  const [, t] = useLanguage()
   const onlineStatus = useOnlineStatus()
   const theme = useTheme()
+
+  useEffect(() => {
+
+  }, [])
 
   return (
     <>
       {props.beforeElements?.map((BeforeElement, i) => (
         <React.Fragment key={i}>
           {BeforeElement}
-        </React.Fragment>))
-      }
+        </React.Fragment>))}
       {props.beforeComponents?.map((BeforeComponent, i) => (
-        <BeforeComponent key={i} {...props} />))
-      }
+        <BeforeComponent key={i} {...props} />))}
       <Container id='container' isOnline={props.isOnline ?? onlineStatus}>
+        <h1>{t('MOBILE_NO_INTERNET', 'No internet connection')}</h1>
+        <p>{t('NETWORK_OFFLINE_MESSAGE', 'Your connection appears to be off-line. Try to refresh the page')}</p>
         <Image>
           <img id='not-network' src={props.image || theme.images?.general?.notNetwork} alt='Not network' width='640' height='320' loading='lazy' />
         </Image>
+        <ButtonWrapper>
+          <Button
+            color='primary'
+            onClick={() => window.location.reload()}
+          >
+            {t('REFRESH', 'Refresh')}
+          </Button>
+        </ButtonWrapper>
       </Container>
       {props.afterComponents?.map((AfterComponent, i) => (
-        <AfterComponent key={i} {...props} />))
-      }
+        <AfterComponent key={i} {...props} />))}
       {props.afterElements?.map((AfterElement, i) => (
         <React.Fragment key={i}>
           {AfterElement}
-        </React.Fragment>))
-      }
+        </React.Fragment>))}
     </>
   )
 }
--- src/components/NotNetworkConnectivity/styles.js
+++ src/components/NotNetworkConnectivity/styles.js
@@ -4,21 +4,50 @@ export const Container = styled.div`
   display: ${({ isOnline }) => isOnline ? 'none' : 'flex'};
   flex-direction: column;
   justify-content: center;
-  align-items: center;
-  width: 100%;
-  margin: 150px auto 100px;
+  width: 90%;
+  margin: 60px auto;
   box-sizing: border-box;
 
+  > h1 {
+    font-size: 24px;
+    line-height: 36px;
+    font-weight: 600;
+    margin: 0;
+  }
+
+  > p {
+    font-size: 14px;
+    line-height: 24px;
+    margin: 13px 0;
+  }
+
   @media (min-width: 768px) {
-    margin: 70px auto;
+    width: 80%;
+  }
+
+  @media (min-width: 1024px) {
+    width: 60%;
   }
 `
 
 export const Image = styled.div`
-  padding-left: 20px;
+  margin-top: 70px;
+  display: flex;
+  justify-content: center;
   img {
+    max-width: 300px;
     width: 100%;
     height: auto;
     aspect-ratio: attr(width) / attr(height)
+  }
+`
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 34px 0;
+  > button {
+    height: 44px;
+    border-radius: 8px;
   }
 `
