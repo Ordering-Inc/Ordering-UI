ad6165d3775bfc0eb1785deb8e40af4393f69e8a
Jesus David <jesusdpch00@gmail.com>
2022-05-13 04:33:27 -0400

    export phone autocomplete

---
+++ src/themes/five/src/components/PhoneAutocomplete/index.js
@@ -0,0 +1,251 @@
+import React, { useState, useEffect } from 'react'
+import Select from 'react-select'
+import {
+  PhoneAutocomplete as PhoneAutocompleteController,
+  useLanguage,
+  useOrder,
+  useCustomer
+} from 'ordering-components'
+import { useTheme } from 'styled-components'
+
+import { Modal } from '../Modal'
+import { SignUpForm } from '../SignUpForm'
+import { Button } from '../../styles/Buttons'
+import { Alert } from '../Confirm'
+import { UserDetails } from '../UserDetails'
+import { AddressList } from '../AddressList'
+
+import {
+  PhoneContainer,
+  ContentWrapper,
+  Title,
+  Slogan,
+  UserEdit,
+  WrappBtn,
+  SelectContainer
+} from './styles'
+
+import MdcCellphoneAndroid from '@meronex/icons/mdc/MdcCellphoneAndroid'
+
+const PhoneAutocompleteUI = (props) => {
+  const {
+    phone,
+    customerState,
+    customersPhones,
+    setCustomersPhones,
+    openModal,
+    setOpenModal,
+    onChangeNumber,
+    setCustomerState,
+    countryCallingCode,
+    onRedirectPage
+  } = props
+  const [orderState] = useOrder()
+  const [, t] = useLanguage()
+  const theme = useTheme()
+  const [, { deleteUserCustomer }] = useCustomer()
+  const [alertState, setAlertState] = useState({ open: false, content: [] })
+  const [inputValue, setInputValue] = useState('')
+  const [optSelected, setOptSelected] = useState(null)
+  const [isOpenUserData, setIsOpenUserData] = useState(false)
+  const [isAddressFormOpen, setIsAddressFormOpen] = useState(false)
+  const userCustomer = JSON.parse(window.localStorage.getItem('user-customer'))
+
+  const userName = userCustomer?.lastname
+    ? `${userCustomer?.name} ${userCustomer?.lastname}`
+    : userCustomer?.name
+
+  const handleCloseAlert = () => {
+    setCustomersPhones({ ...customersPhones, error: null })
+    setAlertState({ open: false, content: [] })
+  }
+
+  const saveCustomerUser = (user) => {
+    setCustomersPhones({ ...customersPhones, users: [...customersPhones.users, user] })
+    setCustomerState({ ...customerState, result: user })
+    setOpenModal({ customer: true, signup: false })
+  }
+
+  const handleFindClick = () => {
+    if (userCustomer?.id && orderState?.options?.address?.address) {
+      onRedirectPage && onRedirectPage('search')
+    } else {
+      setAlertState({ open: true, content: t('SELECT_ADDRESS_CUSTOMER', 'Please select an address for the selected customer') })
+    }
+  }
+
+  useEffect(() => {
+    if (customersPhones?.error) {
+      setAlertState({ open: true, content: [customersPhones?.error] })
+    }
+  }, [customersPhones?.error])
+
+  const onInputChange = (inputValue, { action }) => {
+    if (action === 'menu-close' || action === 'input-blur' || action === 'set-value') {
+      return
+    }
+    if (!inputValue) {
+      setInputValue(inputValue)
+    }
+    if ((inputValue && inputValue.length > 10) || !(/^[0-9]+$/.test(inputValue))) {
+      return
+    }
+    setInputValue(inputValue)
+    onChangeNumber(inputValue)
+  }
+
+  const onChange = (option) => {
+    setOptSelected(option)
+    setInputValue(option ? option?.value : '')
+    const user = customersPhones.users?.find(user => user.cellphone === option?.value || user.phone === option?.value)
+    if (user) {
+      setCustomerState({ ...customerState, result: user })
+      setOpenModal({ ...openModal, customer: true })
+    }
+  }
+
+  const createNewUser = () => {
+    if ((optSelected && optSelected?.value?.length === 10) || (!optSelected && phone.length === 10)) {
+      setOpenModal({ ...openModal, signup: true })
+    } else {
+      setAlertState({
+        open: true,
+        content: t('ERROR_MIN_CHARACTERS_PHONE', 'The Phone / Mobile must be 10 characters')
+      })
+    }
+  }
+
+  const handleCloseAddressList = () => {
+    setOpenModal({ openModal, customer: false })
+    setCustomerState({ ...customerState, result: { error: false } })
+    deleteUserCustomer(true)
+  }
+
+  const optionsToSelect = customersPhones.users.map(user => {
+    const obj = {}
+    obj.value = user.cellphone || user.phone
+    obj.label = `${user?.country_phone_code ? `(${user?.country_phone_code})` : ''} ${user?.phone && !user?.cellphone ? `${user?.phone}` : ''} ${user?.cellphone ? `${user.cellphone}` : ''} - {${user.name}}`
+    return obj
+  }) || []
+
+  return (
+    <>
+      {props.beforeElements?.map((BeforeElement, i) => (
+        <React.Fragment key={i}>
+          {BeforeElement}
+        </React.Fragment>))}
+      {props.beforeComponents?.map((BeforeComponent, i) => (
+        <BeforeComponent key={i} {...props} />))}
+      <PhoneContainer bgimage={theme.images?.general?.homeHero}>
+        <ContentWrapper>
+          <Title>{t('TITLE_HOME_CALLCENTER', 'Welcome to your Ordering Call Center.')}</Title>
+          <Slogan>{t('SUBTITLE_HOME_CALLCENTER', 'Start First by adding the customers\' phone number')}</Slogan>
+          {!userCustomer && (
+            <SelectContainer>
+              <MdcCellphoneAndroid size={26} />
+              <Select
+                isSearchable
+                isClearable
+                className='basic-single'
+                classNamePrefix='select'
+                placeholder={t('PHONE_NUMBER', 'Phone number')}
+                value={optSelected}
+                noOptionsMessage={() => inputValue?.length > 6 ? t('NO_OPTIONS', 'No options') : t('TYPE_AT_LEAST_NUMBER_SUGGEST', 'Type at least 7 numbers for suggesstions')}
+                inputValue={!optSelected ? inputValue : ''}
+                onChange={onChange}
+                onInputChange={onInputChange}
+                isLoading={customersPhones?.loading}
+                options={optionsToSelect}
+              />
+            </SelectContainer>
+          )}
+          <WrappBtn>
+            <Button
+              color={inputValue || (userCustomer && orderState?.options?.address?.address) ? 'primary' : 'secundary'}
+              onMouseDown={() => !(userCustomer && orderState?.options?.address?.address) ? createNewUser() : handleFindClick()}
+              disabled={!inputValue && !(userCustomer && orderState?.options?.address?.address)}
+            >
+              {
+                !(userCustomer && orderState?.options?.address?.address)
+                  ? t('CREATE_CUSTOMER', 'Create new customer')
+                  : `${t('CONTINUE_WITH', 'Continue with')} ${userName}`
+              }
+            </Button>
+          </WrappBtn>
+        </ContentWrapper>
+      </PhoneContainer>
+      <Modal
+        open={openModal.signup}
+        width='80%'
+        onClose={() => setOpenModal({ openModal, signup: false })}
+      >
+        <SignUpForm
+          externalPhoneNumber={`${countryCallingCode} ${optSelected?.value || phone}`}
+          saveCustomerUser={saveCustomerUser}
+          fieldsNotValid={props.fieldsNotValid}
+          useChekoutFileds
+          isCustomerMode={props.isCustomerMode}
+        />
+      </Modal>
+      <Modal
+        open={openModal.customer}
+        width='80%'
+        onClose={() => handleCloseAddressList()}
+        hideCloseDefault
+        padding='20px'
+      >
+        <UserEdit>
+          {!customerState?.loading && (
+            <>
+              <UserDetails
+                isAddressFormOpen={isAddressFormOpen}
+                isOpenUserData={isOpenUserData}
+                userId={customerState?.result?.id}
+                isCustomerMode
+                isModal
+                setIsOpenUserData={setIsOpenUserData}
+                onClose={() => handleCloseAddressList()}
+              />
+              <AddressList
+                isModal
+                userId={customerState?.result?.id}
+                changeOrderAddressWithDefault
+                userCustomerSetup={{
+                  ...customerState?.result,
+                  phone
+                }}
+                isEnableContinueButton
+                isCustomerMode
+                isOpenUserData={isOpenUserData}
+                setIsOpenUserData={setIsOpenUserData}
+                setIsAddressFormOpen={setIsAddressFormOpen}
+              />
+            </>
+          )}
+        </UserEdit>
+      </Modal>
+      <Alert
+        title={t('ERROR', 'Error')}
+        open={alertState.open}
+        content={alertState.content}
+        onClose={handleCloseAlert}
+        onAccept={handleCloseAlert}
+      />
+      {props.afterComponents?.map((AfterComponent, i) => (
+        <AfterComponent key={i} {...props} />))}
+      {props.afterElements?.map((AfterElement, i) => (
+        <React.Fragment key={i}>
+          {AfterElement}
+        </React.Fragment>))}
+    </>
+  )
+}
+
+export const PhoneAutocomplete = (props) => {
+  const phoneProps = {
+    UIComponent: PhoneAutocompleteUI,
+    ...props
+  }
+
+  return <PhoneAutocompleteController {...phoneProps} />
+}
---
+++ src/themes/five/src/components/PhoneAutocomplete/styles.js
@@ -0,0 +1,285 @@
+import styled, { css } from 'styled-components'
+
+export const PhoneContainer = styled.div`
+  width: 100%;
+  height: calc(100vh - 97px);
+
+  ${({ bgimage }) => bgimage && css`
+    background-repeat: no-repeat, repeat;
+    background-size: cover;
+    object-fit: cover;
+    background-position: bottom;
+    background-image: url(${bgimage});
+  `}
+
+  .spinner-content{
+    > div{
+      width: 40px;
+      height: 40px;
+      border-width: 5px;
+    }
+  }
+
+ .autocomplete {
+  /*the container must be positioned relative:*/
+  position: relative;
+  }
+  input {
+    border: 1px solid transparent;
+    background-color: #f1f1f1;
+    padding: 10px;
+    font-size: 16px;
+  }
+  input[type=text] {
+    background-color: #f1f1f1;
+    width: 100%;
+  }
+  input[type=submit] {
+    background-color: #f1f1f1;
+    width: 100%;
+  }
+  .autocomplete-items {
+    position: absolute;
+    border: 1px solid #d4d4d4;
+    border-bottom: none;
+    border-top: none;
+    z-index: 99;
+    /*position the autocomplete items to be the same width as the container:*/
+    top: 100%;
+    left: 0;
+    right: 0;
+    overflow: auto;
+    max-height: 200px
+  }
+  .autocomplete-items div {
+    padding: 10px;
+    cursor: pointer;
+    background-color: #fff;
+    border-bottom: 1px solid #d4d4d4;
+  }
+  .autocomplete-items div:hover {
+    /*when hovering an item:*/
+    background-color: #e9e9e9;
+  }
+  .autocomplete-active {
+    /*when navigating through the items using the arrow keys:*/
+    background-color: DodgerBlue !important;
+    color: #ffffff;
+  }
+
+  @media (min-width: 821px) {
+    height: calc(100vh - 65px);
+  }
+`
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+  padding: 0px 20px 0px;
+
+  .basic-single {
+    margin-bottom: 15px;
+  }
+
+  .select__control {
+    border-radius: 30px;
+  }
+
+  .select__control.select__control--is-focused {
+    border-color: ${props => props.theme?.colors?.primary} !important;
+    box-shadow: 0 0 0 1px ${props => props.theme?.colors?.primary} !important;
+  }
+
+  .select__value-container {
+    padding: 10px 8px 0;
+  }
+  .select__indicator.select__loading-indicator {
+    color: ${props => props.theme?.colors?.primary} !important;
+  }
+
+  .select__indicator.select__dropdown-indicator,
+  .select__indicator-separator {
+    display: none;
+  }
+
+  ${props => props.theme?.rtl && css`
+      padding: 0px 20px 0px;
+    `}
+
+    input {
+      width: 90%;
+      margin-bottom: 15px;
+    }
+
+    @media (min-width: 425px) {
+      input {
+        width: 45%;
+      }
+    }
+
+    @media (min-width: 768px) {
+      padding: 0px 40px 0px;
+
+      ${props => props.theme?.rtl && css`
+        padding: 0px 40px 0px;
+      `}
+    }
+`
+
+export const WrappBtn = styled.div`
+  width: 50%;
+
+  button {
+    min-width: 130px;
+  }
+`
+
+export const Title = styled.h1`
+  margin: 0px;
+  text-align: left;
+  font: normal normal normal 80px ${props => props.theme.fonts.special?.name || 'Georgia'};
+  letter-spacing: 0px;
+  color: #ffffff;
+  text-shadow: 0px 3px 6px #00000029;
+  opacity: 1;
+  font-size: 35px;
+
+  ${props => props.theme?.rtl && css`
+      text-align: right;
+  `}
+`
+
+export const Slogan = styled.p`
+  margin: 0px;
+  text-align: left;
+  font-size: 18px;
+  letter-spacing: 0px;
+  color: #fff;
+  opacity: 1;
+  margin-bottom: 15px;
+
+  ${props => props.theme?.rtl && css`
+      text-align: right;
+  `}
+
+  @media (min-width: 480px) {
+    font-size: 24px;
+  }
+`
+
+export const WrapInput = styled.div`
+  position: relative;
+  cursor: pointer;
+
+  ${({ withIcon }) => withIcon && css`
+    width: calc(100% - 20px);
+    box-sizing: border-box;
+
+    &::before {
+      content: "";
+      position: absolute;
+      right: 5px;
+      top: 0;
+      bottom: 18px;
+      width: 15px;
+
+      ${props => props.theme?.rtl && css`
+        left: 5px;
+        right: initial;
+     `}
+    }
+
+    @media (min-width: 1024px) {
+      width: calc(50% - 20px);
+    }
+  `}
+  
+  div{
+    color: #FFF;
+    position: relative;
+    bottom: 25px;
+    left: 15px;
+    ${props => props.theme?.rtl && css`
+      left: initial;
+      right: 15px;
+    `}
+  }
+
+  svg {
+    color: #FFF;
+    position: absolute;
+    font-size: 22px;
+
+    margin-right: 10px;
+      ${props => props.theme?.rtl && css`
+        margin-left: 10px;
+        margin-right: 0;
+      `}
+
+    ${props => props.theme?.rtl ? css`
+      left: 0px;
+    ` : css`
+      right: 0px;
+    `}
+
+  }
+`
+
+export const AutoComplete = styled.div`
+  width: 45%; 
+  button {
+    position: absolute;
+    left: 100%;
+    transform: translate(-90%, -165%)
+  }
+`
+
+export const UserEdit = styled.div`
+  > :first-child{
+     margin-bottom: 20px;  
+  }
+`
+
+export const Info = styled.p``
+
+export const SelectContainer = styled.div`
+  display: flex;
+  width: 60%;
+  position: relative;
+  align-items: center;
+  svg{
+    position: absolute;
+    top: 0;
+    left: 0;
+    transform: translate(50%, 70%);
+    z-index: 10;
+  }
+  > div {
+    width: 100%;
+    .select__placeholder, .select__single-value, input {
+      margin-left: 40px;
+      z-index: 100
+    }
+    .select__menu {
+      position: relative;
+      padding: 20px;
+    }
+    .select__clear-indicator {
+      position: relative;
+      left: 0;
+      transform: translate(-300%, -165%);
+      svg{
+        width: 30px;
+        height: 30px;
+      }
+    }
+  }
+`
+
+export const SearchContainer = styled.div`
+  position: absolute;
+  top: 0;
+  width: 90%;
+`
