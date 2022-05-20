d648a11fd3d0ad9be638f3e2bf553186cb753d25
Jesus David <50583541+YisusJe@users.noreply.github.com>
2022-05-20 15:30:35 -0400

    [ADD] subcategories that belong to the main category (#1076)
    * [ADD] subcategories that belong to the main category
    
    * format code

--- src/themes/five/src/components/RenderProductsLayout/index.js
+++ src/themes/five/src/components/RenderProductsLayout/index.js
@@ -57,7 +57,10 @@ export const RenderProductsLayout = (props) => {
     handleChangeSearch,
     setOpenBusinessInformation,
     handleCartOpen,
-    isCustomLayout
+    isCustomLayout,
+    setSubcategoriesSelected,
+    subcategoriesSelected,
+    isLazy
   } = props
 
   const theme = useTheme()
@@ -145,6 +148,7 @@ export const RenderProductsLayout = (props) => {
                         { id: 'featured', name: t('FEATURED', theme?.defaultLanguages?.FEATURED || 'Featured') },
                         ...business?.categories.sort((a, b) => a.rank - b.rank)
                       ]}
+                      isLazy={isLazy}
                       category={categorySelected}
                       categoryState={categoryState}
                       businessId={business?.id}
@@ -158,6 +162,9 @@ export const RenderProductsLayout = (props) => {
                       errorQuantityProducts={errorQuantityProducts}
                       business={business}
                       currentCart={currentCart}
+                      setSubcategoriesSelected={setSubcategoriesSelected}
+                      subcategoriesSelected={subcategoriesSelected}
+                      onClickCategory={onClickCategory}
                     />
                   </WrapContent>
                 </BusinessCategoryProductWrapper>
