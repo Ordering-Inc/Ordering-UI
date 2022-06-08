4783885e7758ede4984f27601b71f35554e6f30f
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-06-08 19:48:12 +0800

    [Fix] Google login

---
+++ src/themes/five/src/components/GoogleIdentity/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+import { Button } from '../../styles/Buttons'
+
+export const GoogleButton = styled(Button)`
+  background-color: #EFEFEF;
+  border-color: #EFEFEF;
+  font-size: 16px;
+  font-weight: 400;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 10px;
+  height: 41.6px;
+  svg {
+    font-size: 28px;
+    color: #1977F2;
+  }
+
+  div {
+    margin: 0 auto;
+  }
+`
