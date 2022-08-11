2716a07247b33f93610c0ee76584480abf60ea0b
Jesus David <50583541+YisusJe@users.noreply.github.com>
2022-08-11 18:48:20 -0400

    Web custom design (#1234)
    * [ADD] web custom design
    
    * [UPDATE] theme json

--- src/themes/five/src/components/SingleProductCard/index.js
+++ src/themes/five/src/components/SingleProductCard/index.js
@@ -17,6 +17,7 @@ import {
   RibbonBox,
   TitleWrapper
 } from './styles'
+import { Button } from '../../styles/Buttons'
 
 const SingleProductCardUI = (props) => {
   const {
@@ -55,6 +56,9 @@ const SingleProductCardUI = (props) => {
 
   const maxCartProductConfig = (stateConfig.configs.max_product_amount ? parseInt(stateConfig.configs.max_product_amount) : 100) - totalBalance
 
+  const showAddButton = !theme?.layouts?.business_view?.components?.products?.components?.add_to_cart_button?.hidden
+  const productsRows = theme?.layouts?.business_view?.components?.products?.components?.layout?.rows
+
   let maxCartProductInventory = (product?.inventoried ? product?.quantity : undefined) - totalBalance
   maxCartProductInventory = !isNaN(maxCartProductInventory) ? maxCartProductInventory : maxCartProductConfig
 
@@ -68,83 +72,91 @@ const SingleProductCardUI = (props) => {
       return
     }
     (!isSkeleton && !useCustomFunctionality && onProductClick && onProductClick(product, product?.business?.slug)) ||
-    (useCustomFunctionality && onCustomClick && onCustomClick())
+      (useCustomFunctionality && onCustomClick && onCustomClick())
   }
 
   const handleChangeFavorite = () => {
     handleFavoriteProduct && handleFavoriteProduct(!product?.favorite)
   }
-
+  console.log(productsRows)
   return (
-    <CardContainer
-      ref={$element}
-      soldOut={isSoldOut || maxProductQuantity <= 0}
-      onClick={handleClickProduct}
-      isCartOnProductsList={isCartOnProductsList}
-      style={useCustomFunctionality && customStyle}
-      className='product-card'
-    >
-      {isObservedValidation && (
-        <>
-          {!useCustomFunctionality && (
-            <>
-              {!isSkeleton && productAddedToCartLength > 0 && (
-                <QuantityContainer>
-                  <span>{productAddedToCartLength}</span>
-                </QuantityContainer>
-              )}
-              <CardInfo soldOut={isSoldOut || maxProductQuantity <= 0}>
-                <TitleWrapper>
-                  {!isSkeleton ? (<h1>{product?.name}</h1>) : (<Skeleton width={100} />)}
-                  {!useKioskApp && (
-                    !isSkeleton ? (
-                      <span onClick={() => handleChangeFavorite()} ref={favoriteRef}>
-                        {product?.favorite ? <Like /> : <DisLike />}
-                      </span>
-                    ) : (<Skeleton width={16} height={16} />)
-                  )}
-                </TitleWrapper>
-                {!isSkeleton ? (
-                  <PriceWrapper>
-                    <span>{product?.price ? parsePrice(product?.price) : ''}</span>
-                    {!(isSoldOut || maxProductQuantity <= 0) && (
-                      <span className='off-price'>{product?.offer_price && product?.in_offer ? parsePrice(product?.offer_price) : ''}</span>
-                    )}
-                  </PriceWrapper>
-                ) : (
-                  <Skeleton width={100} />
+    <>
+      <CardContainer
+        ref={$element}
+        soldOut={isSoldOut || maxProductQuantity <= 0}
+        onClick={handleClickProduct}
+        isCartOnProductsList={isCartOnProductsList}
+        style={useCustomFunctionality && customStyle}
+        className='product-card'
+        productsRows={productsRows}
+      >
+        {isObservedValidation && (
+          <div>
+            {!useCustomFunctionality && (
+              <>
+                {!isSkeleton && productAddedToCartLength > 0 && (
+                  <QuantityContainer>
+                    <span>{productAddedToCartLength}</span>
+                  </QuantityContainer>
                 )}
-                {!isSkeleton ? (<p>{product?.description}</p>) : (<Skeleton width={100} />)}
-              </CardInfo>
-              {!isSkeleton ? (
-                <WrapLogo>
-                  {product?.ribbon?.enabled && (
-                    <RibbonBox
-                      bgColor={product?.ribbon?.color}
-                      isRoundRect={product?.ribbon?.shape === shape?.rectangleRound}
-                      isCapsule={product?.ribbon?.shape === shape?.capsuleShape}
-                    >
-                      {product?.ribbon?.text}
-                    </RibbonBox>
+                <CardInfo soldOut={isSoldOut || maxProductQuantity <= 0}>
+                  <TitleWrapper>
+                    {!isSkeleton ? (<h1>{product?.name}</h1>) : (<Skeleton width={100} />)}
+                    {!useKioskApp && (
+                      !isSkeleton ? (
+                        <span onClick={() => handleChangeFavorite()} ref={favoriteRef}>
+                          {product?.favorite ? <Like /> : <DisLike />}
+                        </span>
+                      ) : (<Skeleton width={16} height={16} />)
+                    )}
+                  </TitleWrapper>
+                  {!isSkeleton ? (
+                    <PriceWrapper>
+                      <span>{product?.price ? parsePrice(product?.price) : ''}</span>
+                      {!(isSoldOut || maxProductQuantity <= 0) && (
+                        <span className='off-price'>{product?.offer_price && product?.in_offer ? parsePrice(product?.offer_price) : ''}</span>
+                      )}
+                    </PriceWrapper>
+                  ) : (
+                    <Skeleton width={100} />
                   )}
-                  <CardLogo
-                    className='image'
-                    soldOut={isSoldOut || maxProductQuantity <= 0}
-                    bgimage={optimizeImage(product?.images || theme.images?.dummies?.product, 'h_200,c_limit')}
-                  />
-                </WrapLogo>
-              ) : (
-                <Skeleton height={75} width={75} />
-              )}
-              {(isSoldOut || maxProductQuantity <= 0) && <SoldOut isBottom={product?.ribbon?.enabled}>{t('SOLD_OUT', 'SOLD OUT')}</SoldOut>}
-            </>
-          )}
-          {useCustomFunctionality && customText && (
-            <span style={{ fontSize: 16, fontWeight: 500 }}>{customText}</span>
-          )}
-        </>
-      )}
-    </CardContainer>
+                  {!isSkeleton ? (<p>{product?.description}</p>) : (<Skeleton width={100} />)}
+                </CardInfo>
+                {!isSkeleton ? (
+                  <WrapLogo>
+                    {product?.ribbon?.enabled && (
+                      <RibbonBox
+                        bgColor={product?.ribbon?.color}
+                        isRoundRect={product?.ribbon?.shape === shape?.rectangleRound}
+                        isCapsule={product?.ribbon?.shape === shape?.capsuleShape}
+                      >
+                        {product?.ribbon?.text}
+                      </RibbonBox>
+                    )}
+                    <CardLogo
+                      className='image'
+                      soldOut={isSoldOut || maxProductQuantity <= 0}
+                      bgimage={optimizeImage(product?.images || theme.images?.dummies?.product, 'h_200,c_limit')}
+                    />
+                  </WrapLogo>
+                ) : (
+                  <Skeleton height={75} width={75} />
+                )}
+                {(isSoldOut || maxProductQuantity <= 0) && <SoldOut isBottom={product?.ribbon?.enabled}>{t('SOLD_OUT', 'SOLD OUT')}</SoldOut>}
+              </>
+            )}
+            {useCustomFunctionality && customText && (
+              <span style={{ fontSize: 16, fontWeight: 500 }}>{customText}</span>
+            )}
+          </div>
+        )}
+        {!useCustomFunctionality && showAddButton && !isSkeleton && (
+          <Button outline color='primary'>
+            {t('ADD', 'Add')}
+          </Button>
+        )}
+      </CardContainer>
+    </>
   )
 }
 
