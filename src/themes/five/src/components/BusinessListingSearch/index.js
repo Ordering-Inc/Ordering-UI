eaa2836abf91d0e9cc785908efcddcb4e7146be4
Min <skyandarti@outlook.com>
2022-08-20 06:32:20 +0800

    Update: hidden previous order

--- src/themes/five/src/components/BusinessListingSearch/index.js
+++ src/themes/five/src/components/BusinessListingSearch/index.js
@@ -37,7 +37,7 @@ import {
 import Skeleton from 'react-loading-skeleton'
 import { Check2, XLg as Close } from 'react-bootstrap-icons'
 import { SearchBar } from '../SearchBar'
-import { useLanguage, useOrder, useUtils, BusinessSearchList } from 'ordering-components'
+import { useLanguage, useOrder, useUtils, useSession, BusinessSearchList } from 'ordering-components'
 import { BusinessController } from '../BusinessController'
 import { AutoScroll } from '../AutoScroll'
 import { BusinessTypeFilter } from '../BusinessTypeFilter'
@@ -74,6 +74,7 @@ export const BusinessListingSearchUI = (props) => {
   const [orderState] = useOrder()
   const [, t] = useLanguage()
   const theme = useTheme()
+  const [{ auth }] = useSession()
   const [curProduct, setCurProduct] = useState({ business: null, product: null })
   const [{ parsePrice, optimizeImage, parseDistance }] = useUtils()
   const { width } = useWindowSize()
@@ -254,14 +255,16 @@ export const BusinessListingSearchUI = (props) => {
           </TagsContainer>
         </Filters>
         <FiltersResultContainer>
-          <PreviouslyOrderedContainer>
-            <MyOrders
-              hideOrders
-              businessesSearchList={businessesSearchList}
-              onRedirectPage={onRedirectPage}
-              onProductRedirect={onProductRedirect}
-            />
-          </PreviouslyOrderedContainer>
+          {auth && (
+            <PreviouslyOrderedContainer>
+              <MyOrders
+                hideOrders
+                businessesSearchList={businessesSearchList}
+                onRedirectPage={onRedirectPage}
+                onProductRedirect={onProductRedirect}
+              />
+            </PreviouslyOrderedContainer>
+          )}
           <BusinessListWrapper>
             {businessesSearchList.businesses?.length > 0 && (
               <h2>{t('BUSINESSES', 'Businesses')}</h2>
