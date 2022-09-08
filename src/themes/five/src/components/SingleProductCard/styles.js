42a13a78a77dc33de758a7afdeb825dc761bf90c
MingXue <mingxue@ordering.co>
2022-09-08 08:38:05 +0800

    add style for button in singleProductCart (#1281)
    * add style for button in singleProductCart
    
    * rename props name
    
    * rename singleproduct props
    
    * improve code style

--- src/themes/five/src/components/SingleProductCard/styles.js
+++ src/themes/five/src/components/SingleProductCard/styles.js
@@ -2,8 +2,10 @@ import React from 'react'
 import styled, { css } from 'styled-components'
 
 export const CardContainer = styled.div`
-  min-height: 162px;
-  max-height: 162px;
+  ${({ isShowAddButt }) => css`
+    min-height: ${isShowAddButt ? '162px' : '110px'};
+    max-height: ${isShowAddButt ? '162px' : '110px'};
+  `}
   background: ${({ soldOut }) => soldOut ? '#6c757d33' : '#FFF'};
   border: 1px solid #E9ECEF;
   padding: 10px;
