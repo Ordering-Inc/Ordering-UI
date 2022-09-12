c09e3efb3f1683aba48a735db165578a84b350f9
MingXue <mingxue@ordering.co>
2022-09-13 05:17:22 +0800

    update order detail style

--- src/themes/five/src/components/OrderDetails/styles.js
+++ src/themes/five/src/components/OrderDetails/styles.js
@@ -6,7 +6,11 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: 20px 0;
+  margin: 45px 0 20px;
+
+  @media (min-width: 740px) {
+    margin: 20px 0;
+  }
 `
 
 export const WrapperContainer = styled.div`
