fd83edcffbf4f243a8c14a68a203ea59dbeb9b4a
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-10-25 10:47:27 -0500

    [Fix] template 5 feedback (#1364)
    * [Fix] crash bug in Stripe connect
    
    * [Fix] business redirect

--- template/pages/OrderDetails/index.js
+++ template/pages/OrderDetails/index.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { useHistory, useParams, useLocation } from 'react-router-dom'
 import { HelmetTags } from '../../components/HelmetTags'
-import { useOrderingTheme } from 'ordering-components'
+import { useOrderingTheme, useSite, useEvent } from 'ordering-components'
 import { OrderDetails } from '../../../src/themes/five/src/components/OrderDetails'
 import { OrderDetails as OrderDetailsKiosk } from '../../../src/themes/five/src/components/OrderDetails/layouts/Kiosk'
 import { OrderDetails as OrderDetailsOld } from '../../../src/components/OrderDetails'
 import settings from '../../config'
+import { checkSiteUrl } from '../../Utils'
 
 export const OrderDetailsPage = (props) => {
   const history = useHistory()
   const { orderId } = useParams()
   const hashKey = new URLSearchParams(useLocation()?.search)?.get('hash') || null
   const [orderingTheme] = useOrderingTheme()
+  const [events] = useEvent()
+  const [{ site }] = useSite()
+  const businessUrlTemplate = checkSiteUrl(site?.business_url_template, '/store/:business_slug')
+
   const useKioskApp = settings?.use_kiosk
   const OrderDetailsComponent = useKioskApp ? OrderDetailsKiosk
     : orderingTheme?.theme?.confirmation?.components?.layout?.type === 'old'
@@ -27,7 +32,11 @@ export const OrderDetailsPage = (props) => {
       history.push('/profile/orders')
     },
     handleBusinessRedirect: (slug) => {
-      history.push(`/store/${slug}`)
+      if (businessUrlTemplate === '/store/:business_slug' || businessUrlTemplate === '/:business_slug') {
+        events.emit('go_to_page', { page: 'business', params: { business_slug: slug } })
+      } else {
+        events.emit('go_to_page', { page: 'business', search: `?${businessUrlTemplate.split('?')[1].replace(':business_slug', '')}${slug}` })
+      }
     }
   }
   return (
