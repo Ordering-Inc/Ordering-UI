47e52c141f15a9aa4b18c97e4e892ebd008a8aff
Jesus David <50583541+YisusJe@users.noreply.github.com>
2022-11-29 10:45:41 -0400

    Left layout improv (#1433)
    * [ADD] improvements for left layout businesses
    
    * [fix] scroll behavior
    
    * [add] new design for left layout
    
    Co-authored-by: DESKTOP-S4I8S56\Jesus <jesus@ordering.co>

--- src/themes/five/src/components/AccordionDropdown/styles.js
+++ src/themes/five/src/components/AccordionDropdown/styles.js
@@ -19,10 +19,34 @@ export const CategoryTab = styled.div`
   }
 
   span {
-    width: 90%;
+    width: 9
+    0%;
+  }
+
+  > div {
+    display: flex;
+    align-items: center;
   }
 
   ${({ active }) => active && css`
     background-color: ${props => props.theme.colors.primaryContrast};
   `}
+  
+`
+
+export const SelectCategory = styled.div`
+  width: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 30px;
+  border: 1px solid ${({ theme }) => theme?.colors?.primary};
+  margin-right: 10px;
+  height: 20px;
+  svg {
+    color: ${({ theme }) => theme?.colors?.primary}
+  }
+  &:hover{
+    opacity: 0.8;
+  }
 `
