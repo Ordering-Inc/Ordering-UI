7257f2eb95895be9f2ad5fd8bd431b910acc8819
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-11-30 18:18:11 -0600

    remove business type

--- src/themes/five/src/components/BusinessBasicInformation/index.js
+++ src/themes/five/src/components/BusinessBasicInformation/index.js
@@ -82,7 +82,6 @@ export const BusinessBasicInformation = (props) => {
   const showLogo = !orderingTheme?.theme?.business_view?.components?.header?.components?.business?.components?.logo?.hidden
   const showDeliveryFee = !orderingTheme?.theme?.business_view?.components?.header?.components?.business?.components?.fee?.hidden
   const showTime = !orderingTheme?.theme?.business_view?.components?.header?.components?.business?.components?.time?.hidden
-  const showBusinessInfo = !orderingTheme?.theme?.business_view?.components?.header?.components?.business?.components?.business_info?.hidden
   const showReviews = !orderingTheme?.theme?.business_view?.components?.header?.components?.business?.components?.reviews?.hidden
   const showDistance = !orderingTheme?.theme?.business_view?.components?.header?.components?.business?.components?.distance?.hidden
   const showSort = !orderingTheme?.theme?.business_view?.components?.header?.components?.business?.components?.sort?.hidden
@@ -205,15 +204,6 @@ export const BusinessBasicInformation = (props) => {
                 </TitleWrapper>
               ) : (
                 <Skeleton width={isCustomerMode ? 100 : 150} height={isCustomerMode ? 35 : 'auto'} />
-              )}
-              {showBusinessInfo && (
-                <>
-                  {!loading ? (
-                    <p className='type'>{getBusinessType()}</p>
-                  ) : (
-                    <Skeleton width={isCustomerMode ? 100 : 150} />
-                  )}
-                </>
               )}
               {typeof hideCity !== 'undefined' && !hideCity && business?.city?.name && (
                 <>
