e2e884d7b1b2431f3b32df3943639a3b1bc2a1bb
Israel Rojas <israelrojas626@gmail.com>
2022-12-25 16:10:07 -0400

    add status

--- src/themes/five/src/components/OrderDetails/index.js
+++ src/themes/five/src/components/OrderDetails/index.js
@@ -574,7 +574,7 @@ const OrderDetailsUI = (props) => {
                           style={{ fontSize: 14 }}
                           color={order?.status === 20 ? 'secundary' : 'primary'}
                           onClick={() => handleChangeOrderStatus(20)}
-                          disabled={order?.status === 20 || order?.status === 15 || order?.status === 1}
+                          disabled={order?.status === 20 || order?.status === 21 || order?.status === 15 || order?.status === 1}
                         >
                           {getOrderStatus(20)?.value}
                         </Button>
