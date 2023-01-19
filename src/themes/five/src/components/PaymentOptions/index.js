e1b618010acfa2617b48dd321da12d2033cfc38b
Jesus David <50583541+YisusJe@users.noreply.github.com>
2023-01-18 21:40:18 -0400

    [FIX] paypal driver tips required (#1518)
    Co-authored-by: DESKTOP-S4I8S56\Jesus <jesus@ordering.co>

--- src/themes/five/src/components/PaymentOptions/index.js
+++ src/themes/five/src/components/PaymentOptions/index.js
@@ -11,7 +11,8 @@ import {
   PaymentOptions as PaymentOptionsController,
   useLanguage,
   useOrder,
-  useSession
+  useSession,
+  useValidationFields
 } from 'ordering-components'
 
 import { Modal } from '../Modal'
@@ -101,8 +102,9 @@ const PaymentOptionsUI = (props) => {
   } = props
   const [, t] = useLanguage()
   const [{ token }] = useSession()
+  const [{ options }] = useOrder()
   const [alertState, setAlertState] = useState({ open: false, content: [] })
-
+  const [validationFields] = useValidationFields()
   const paymethodSelected = props.paySelected || props.paymethodSelected
 
   const methodsPay = ['google_pay', 'apple_pay']
@@ -117,11 +119,25 @@ const PaymentOptionsUI = (props) => {
   const supportedMethods = list?.filter(p => useKioskApp ? includeKioskPaymethods.includes(p.gateway) : p)
 
   const handlePaymentMethodClick = (paymethod) => {
+    if (paymethod?.gateway === 'paypal' &&
+      options.type === 1 &&
+      validationFields?.fields?.checkout?.driver_tip?.enabled &&
+      validationFields?.fields?.checkout?.driver_tip?.required &&
+      (Number(cart?.driver_tip) <= 0)
+    ) {
+      setAlertState({
+        open: true,
+        content: [t('DRIVER_TIPS_REQUIRED', 'Driver tips is required, please select a driver tip before select this paymethod')]
+      })
+      return
+    }
+
     if (cart?.balance > 0) {
       const isPopupMethod = popupMethods.includes(paymethod?.gateway)
       handlePaymethodClick(paymethod, isPopupMethod)
       return
     }
+
     setAlertState({
       open: true,
       content: [t('CART_BALANCE_ZERO', 'Sorry, the amount to pay is equal to zero and it is not necessary to select a payment method')]
