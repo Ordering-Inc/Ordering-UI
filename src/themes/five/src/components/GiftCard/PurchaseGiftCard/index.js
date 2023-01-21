c3369a514e5c94293880f31503e01042792acaf2
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2023-01-22 01:12:10 +0800

    Gift card feedback (#1521)
    * [Update] hide for empty info
    
    * [Update] purchase gift card style
    
    * [Update] purchase gift card UI

--- src/themes/five/src/components/GiftCard/PurchaseGiftCard/index.js
+++ src/themes/five/src/components/GiftCard/PurchaseGiftCard/index.js
@@ -4,12 +4,15 @@ import Skeleton from 'react-loading-skeleton'
 import { Button } from '../../../styles/Buttons'
 import RiRadioButtonFill from '@meronex/icons/ri/RiRadioButtonFill'
 import MdRadioButtonUnchecked from '@meronex/icons/md/MdRadioButtonUnchecked'
+import { NotFoundSource } from '../../NotFoundSource'
 
 import {
   Container,
   GiftCardsListContainer,
+  GiftCardsWrapper,
   GiftCardItem,
-  IconControl
+  IconControl,
+  ActionButtonContainer
 } from './styles'
 
 const PurchaseGiftCardUI = (props) => {
@@ -24,39 +27,46 @@ const PurchaseGiftCardUI = (props) => {
   return (
     <Container>
       <h2>{t('PURCHASE_GIFT_CARD', 'Purchase gift card')}</h2>
-      <p>{t('SELECT_ONE_OPTION', 'Select one option')}</p>
       <GiftCardsListContainer>
-        {productsListState.loading && (
-          [...Array(5).keys()].map(i => (
-            <GiftCardItem key={i}>
-              <Skeleton width={150} />
+        <p>{t('SELECT_ONE_OPTION', 'Select one option')}</p>
+        <GiftCardsWrapper>
+          {productsListState.loading && (
+            [...Array(5).keys()].map(i => (
+              <GiftCardItem key={i}>
+                <Skeleton width={150} />
+              </GiftCardItem>
+            ))
+          )}
+          {productsListState.products.map(product => (
+            <GiftCardItem
+              key={product.id}
+              onClick={() => setSelectedProduct(product)}
+            >
+              <IconControl>
+                {selectedProduct?.id === product.id ? (
+                  <RiRadioButtonFill />
+                ) : (
+                  <MdRadioButtonUnchecked disabled />
+                )}
+              </IconControl>
+              <span>{product.name}</span>
             </GiftCardItem>
-          ))
-        )}
-        {productsListState.products.map(product => (
-          <GiftCardItem
-            key={product.id}
-            onClick={() => setSelectedProduct(product)}
-          >
-            <IconControl>
-              {selectedProduct?.id === product.id ? (
-                <RiRadioButtonFill />
-              ) : (
-                <MdRadioButtonUnchecked disabled />
-              )}
-            </IconControl>
-            <span>{product.name}</span>
-          </GiftCardItem>
-        ))}
+          ))}
+          {!productsListState.loading && productsListState.products.length === 0 && (
+            <NotFoundSource />
+          )}
+        </GiftCardsWrapper>
       </GiftCardsListContainer>
-      <Button
-        type='button'
-        color='primary'
-        disabled={!selectedProduct}
-        onClick={() => handleAccept()}
-      >
-        {t('ACCEPT', 'Accept')}
-      </Button>
+      <ActionButtonContainer>
+        <Button
+          type='button'
+          color='primary'
+          disabled={!selectedProduct}
+          onClick={() => handleAccept()}
+        >
+          {t('ACCEPT', 'Accept')}
+        </Button>
+      </ActionButtonContainer>
     </Container>
   )
 }
