b8cb0632ff0b23cd6680531179334acc439ed6e6
Jesus David <50583541+YisusJe@users.noreply.github.com>
2023-02-08 10:07:25 -0400

    [fix] double driver tip on checkout (#1554)
    Co-authored-by: DESKTOP-S4I8S56\Jesus <jesus@ordering.co>

--- src/themes/five/src/components/Cart/index.js
+++ src/themes/five/src/components/Cart/index.js
@@ -493,6 +493,7 @@ const CartUI = (props) => {
                   validationFields?.fields?.checkout?.driver_tip?.enabled &&
                   driverTipsOptions.length > 0 &&
                   !useKioskApp &&
+                  !isCheckout &&
                   (
                     <>
                       <DriverTipContainer>
