348618463edce3347492179145b273901dac69a2
DESKTOP-S4I8S56\Jesus <jesus@ordering.co>
2023-02-08 12:38:49 -0400

    [ADD] spoonity autologin

--- template/app.js
+++ template/app.js
@@ -32,6 +32,7 @@ import { orderingThemeUpdated } from './components/OrderingThemeUpdated'
 
 import { SpinnerLoader } from '../src/components/SpinnerLoader'
 import { Input } from '../src/themes/five/src/styles/Inputs'
+import { QueryLoginSpoonity } from '../src/themes/five/src/components/QueryLoginSpoonity'
 
 const Header = loadable(() => import('../src/themes/five/src/components/Header'))
 const HeaderKiosk = loadable(() => import('../src/themes/five/src/components/Header/layouts/Kiosk'))
@@ -115,6 +116,12 @@ export const App = () => {
   const hashKey = new URLSearchParams(useLocation()?.search)?.get('hash') || null
   const isKioskApp = settings?.use_kiosk
   const enabledPoweredByOrdering = configs?.powered_by_ordering_module?.value
+
+  let queryToken
+  if (location.search) {
+    const query = new URLSearchParams(location.search)
+    queryToken = query.get('spoonity_token')
+  }
 
   const themeUpdated = {
     ...theme,
@@ -493,9 +500,11 @@ export const App = () => {
                     ) : (
                       isKioskApp
                         ? <HomePage />
-                        : (orderStatus.options?.address?.location || isAllowUnaddressOrderType)
-                          ? <Redirect to={singleBusinessConfig.isActive ? `/${singleBusinessConfig.businessSlug}` : '/search'} />
-                          : <HomePage />
+                        : queryToken
+                          ? <QueryLoginSpoonity token={queryToken} />
+                          : (orderStatus.options?.address?.location || isAllowUnaddressOrderType)
+                            ? <Redirect to={singleBusinessConfig.isActive ? `/${singleBusinessConfig.businessSlug}` : '/search'} />
+                            : <HomePage />
                     )}
                   </Route>
                   <Route exact path='/wallets'>
