b6d692c34f4574c4e7f599d4fe30cb709b602bb2
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2023-02-16 22:20:57 -0400

    [FEAT] mulitcheckout cart (#1568)
    Co-authored-by: Carlos Quiroz <carloseqv29596@gmail.com>

--- src/themes/five/src/components/Cart/index.js
+++ src/themes/five/src/components/Cart/index.js
@@ -100,6 +100,7 @@ const CartUI = (props) => {
   const [openChangeStore, setOpenChangeStore] = useState(false)
 
   const businessUrlTemplate = site?.business_url_template || '/store/:business_slug'
+  const isChewLayout = theme?.header?.components?.layout?.type?.toLowerCase() === 'chew'
 
   const isCouponEnabled = validationFields?.fields?.checkout?.coupon?.enabled
   const cateringTypes = [7, 8]
@@ -449,7 +450,7 @@ const CartUI = (props) => {
                         </tr>
                       ))
                     }
-                    {orderState?.options?.type === 1 && cart?.delivery_price > 0 && cart?.delivery_price_with_discount >= 0 && !hideDeliveryFee && (
+                    {orderState?.options?.type === 1 && cart?.delivery_price > 0 && cart?.delivery_price_with_discount >= 0 && !hideDeliveryFee && isChewLayout && (
                       <tr>
                         <td>{t('DELIVERY_FEE_AFTER_DISCOUNT', 'Delivery Fee After Discount')}</td>
                         <td>{parsePrice(cart?.delivery_price_with_discount)}</td>
