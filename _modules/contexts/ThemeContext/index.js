680d9edfef4ca751fde3b0bd3909ba6c8ac45ebe
carlosg-ordering <carlos.g@ordering.co>
2023-02-23 15:44:09 -0600

    build

--- _modules/contexts/ThemeContext/index.js
+++ _modules/contexts/ThemeContext/index.js
@@ -67,6 +67,7 @@ var ThemeProvider = function ThemeProvider(_ref) {
         font.rel = 'stylesheet';
         font.async = true;
         font.defer = true;
+        font.name = fontFamily.name;
         font.href = fontFamily.href || "https://fonts.googleapis.com/css2?family=".concat(fontFamily.name, ":wght@").concat(fontFamily.weights.join(';'), "&display=swap");
         window.document.body.appendChild(font);
       }
