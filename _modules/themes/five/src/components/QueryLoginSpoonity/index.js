327e27d3635b2206e4a23d942e45b4bae2303826
Carlos Quiroz <carloseqv29596@gmail.com>
2023-02-27 16:26:23 -0400

    build

--- _modules/themes/five/src/components/QueryLoginSpoonity/index.js
+++ _modules/themes/five/src/components/QueryLoginSpoonity/index.js
@@ -21,12 +21,13 @@ function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len 
 function _iterableToArrayLimit(arr, i) { var _i = null == arr ? null : "undefined" != typeof Symbol && arr[Symbol.iterator] || arr["@@iterator"]; if (null != _i) { var _s, _e, _x, _r, _arr = [], _n = !0, _d = !1; try { if (_x = (_i = _i.call(arr)).next, 0 === i) { if (Object(_i) !== _i) return; _n = !1; } else for (; !(_n = (_s = _x.call(_i)).done) && (_arr.push(_s.value), _arr.length !== i); _n = !0); } catch (err) { _d = !0, _e = err; } finally { try { if (!_n && null != _i.return && (_r = _i.return(), Object(_r) !== _r)) return; } finally { if (_d) throw _e; } } return _arr; } }
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 var QueryLoginSpoonityUI = function QueryLoginSpoonityUI(props) {
+  var userState = props.userState;
   var _useLanguage = (0, _orderingComponents.useLanguage)(),
     _useLanguage2 = _slicedToArray(_useLanguage, 2),
     t = _useLanguage2[1];
-  return /*#__PURE__*/_react.default.createElement(_SpinnerLoader.SpinnerLoader, {
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, userState !== null && userState !== void 0 && userState.loading ? /*#__PURE__*/_react.default.createElement(_SpinnerLoader.SpinnerLoader, {
     content: t('LOADING_DELICIOUS_FOOD', 'Loading Ordering Dashboard...')
-  });
+  }) : null);
 };
 var QueryLoginSpoonity = function QueryLoginSpoonity(props) {
   var queryProps = _objectSpread(_objectSpread({}, props), {}, {
