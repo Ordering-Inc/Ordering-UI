03455d207058c059c2e4e81d86492a2049219ec2
carlosg-ordering <carlos.g@ordering.co>
2023-04-13 16:25:48 -0600

    [FEAT] missing exports

--- src/themes/five/index.js
+++ src/themes/five/index.js
@@ -115,6 +115,9 @@ import { ActionsSection } from './src/components/OrderDetails/ActionsSection'
 import { ProductShare } from './src/components/ProductShare'
 import { MultiCart } from './src/components/MultiCart'
 import { QueryLoginSpoonity } from './src/components/QueryLoginSpoonity'
+import { Confirm } from './src/components/Confirm'
+import { TaxInformation } from './src/components/TaxInformation'
+import { SendGiftCard } from './src/components/GiftCard/SendGiftCard'
 
 import { Button } from './src/styles/Buttons'
 import { Input, TextArea } from './src/styles/Inputs'
@@ -241,6 +244,9 @@ export {
   ProductShare,
   MultiCart,
   QueryLoginSpoonity,
+  Confirm,
+  TaxInformation,
+  SendGiftCard,
   // styles
   Button,
   Input,
