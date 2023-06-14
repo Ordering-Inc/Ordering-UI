3901470ac2ec91829b553431056bf3a3f9679eb8
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2023-06-15 04:45:56 +0800

    Show Banner for call center (#1749)
    * [Update] show banner in call center
    
    * [Update] show banner for call center

--- src/themes/five/src/components/BusinessesListing/layouts/OriginalBusinessesListing/index.js
+++ src/themes/five/src/components/BusinessesListing/layouts/OriginalBusinessesListing/index.js
@@ -223,9 +223,7 @@ const BusinessesListingUI = (props) => {
 
   return (
     <BusinessContainer>
-      {!isCustomerMode && ( // Keep this banner at the top
-        <PageBanner position='web_business_listing' />
-      )}
+      <PageBanner position='web_business_listing' />
 
       {(windowSize.width < 576 || (configs?.business_listing_hide_image?.value !== '1' && !isChew)) && (
         <BusinessBanner>
