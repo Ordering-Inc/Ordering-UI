3cb3e69617d935f43e986887ec5300ffe1d78d8f
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2023-07-10 12:59:35 -0600

    [Update] no gift cards

--- src/themes/five/src/components/OrdersOption/index.js
+++ src/themes/five/src/components/OrdersOption/index.js
@@ -365,6 +365,7 @@ export const OrdersOption = (props) => {
   const orderListProps = {
     ...props,
     UIComponent: OrdersOptionUI,
+    noGiftCardOrders: true,
     orderStatus: getAllOrders
       ? [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
       : props.activeOrders
