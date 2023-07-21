f4a6b6e394d83b8e01ca2eaf79bb974442ab14eb
carlosg-ordering <carlos.g@ordering.co>
2023-07-21 11:36:24 -0600

    [FEAT] add cash simbol con va por mi cuenta

--- src/themes/five/src/components/VaXMiCuenta/index.js
+++ src/themes/five/src/components/VaXMiCuenta/index.js
@@ -17,17 +17,17 @@ export const VaXMiCuenta = (props) => {
         <h3>¿Deseas donar a <div><img src='https://d2gjwc6pypyhyf.cloudfront.net/va-por-mi-cuenta.svg'></img></div> ?</h3>
         <p>{t('VA_POR_MI_CUENTA','VA_POR_MI_CUENTA')} <a href={t('VA_POR_MI_CUENTA_URL','VA_POR_MI_CUENTA')} target="_blank"> {t('VA_POR_MI_CUENTA_URL_NAME','VA_POR_MI_CUENTA_URL_NAME')}</a></p>
         <div>
-            {Object.values(defaultOptionsVaXMiCuenta?.donation_values[0]).map((value, index) => (
-              <div key={index}>
-                <Button
-                  value={value} 
-                  isSelected={value === vaXMiCuenta.selectedOption.amount}
-                  onClick={() => handleChangeVaXMiCuenta(value, index)}
-                >
-                  {value === 0 ? 'No Donar': value}
-                </Button>
-              </div>
-            ))}
+          {Object.values(defaultOptionsVaXMiCuenta?.donation_values[0]).map((value, index) => (
+            <div key={index}>
+              <Button
+                value={value} 
+                isSelected={value === vaXMiCuenta.selectedOption.amount}
+                onClick={() => handleChangeVaXMiCuenta(value, index)}
+              >
+                {value === 0 ? 'No Donar' : `$${value}`}
+              </Button>
+            </div>
+          ))}
         </div>
       </div>
     </VaXMiCuentaContainer>
