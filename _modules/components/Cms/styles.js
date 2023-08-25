844f3cb7722747d3885439a1758529b2178ac3bf
carlosg-ordering <carlos.g@ordering.co>
2023-08-25 12:19:31 -0600

    build

--- _modules/components/Cms/styles.js
+++ _modules/components/Cms/styles.js
@@ -12,21 +12,33 @@ function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 var CmsError = _styledComponents.default.div(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n  display: flex;\n  flex-direction: column;\n  align-items: center;\n  justify-content: center;\n  height: 100%;\n  h2{\n    color: #D81212\n  }\n"])));
 exports.CmsError = CmsError;
-var CmsContainer = _styledComponents.default.div(_templateObject2 || (_templateObject2 = _taggedTemplateLiteral(["\n  padding: 0 30px;\n"])));
+var CmsContainer = _styledComponents.default.div(_templateObject2 || (_templateObject2 = _taggedTemplateLiteral(["\n  padding: 0 25px;\n  background: ", " !important;\n  > div * {\n    font-family: ", " !important;\n    background: transparent !important;\n    color: ", " !important;\n  }\n"])), function (_ref) {
+  var _theme$colors$tertiar, _theme$colors;
+  var theme = _ref.theme;
+  return (_theme$colors$tertiar = theme === null || theme === void 0 ? void 0 : (_theme$colors = theme.colors) === null || _theme$colors === void 0 ? void 0 : _theme$colors.tertiaryContrast) !== null && _theme$colors$tertiar !== void 0 ? _theme$colors$tertiar : '#000';
+}, function (_ref2) {
+  var _theme$fonts, _theme$fonts$special;
+  var theme = _ref2.theme;
+  return theme === null || theme === void 0 ? void 0 : (_theme$fonts = theme.fonts) === null || _theme$fonts === void 0 ? void 0 : (_theme$fonts$special = _theme$fonts.special) === null || _theme$fonts$special === void 0 ? void 0 : _theme$fonts$special.name;
+}, function (_ref3) {
+  var _theme$colors$tertiar2, _theme$colors2;
+  var theme = _ref3.theme;
+  return (_theme$colors$tertiar2 = theme === null || theme === void 0 ? void 0 : (_theme$colors2 = theme.colors) === null || _theme$colors2 === void 0 ? void 0 : _theme$colors2.tertiary) !== null && _theme$colors$tertiar2 !== void 0 ? _theme$colors$tertiar2 : '#FFF';
+});
 exports.CmsContainer = CmsContainer;
-var SkeletonContainer = _styledComponents.default.div(_templateObject3 || (_templateObject3 = _taggedTemplateLiteral(["\n  display: flex;\n  flex-direction: column\n"])));
+var SkeletonContainer = _styledComponents.default.div(_templateObject3 || (_templateObject3 = _taggedTemplateLiteral(["\n  display: flex;\n  flex-direction: column;\n"])));
 exports.SkeletonContainer = SkeletonContainer;
 var SkeletonHeader = _styledComponents.default.div(_templateObject4 || (_templateObject4 = _taggedTemplateLiteral(["\n  width: 100%;\n  height: 100px;\n  margin: 20px auto;\n"])));
 exports.SkeletonHeader = SkeletonHeader;
 var SkeletonContent = _styledComponents.default.div(_templateObject5 || (_templateObject5 = _taggedTemplateLiteral(["\n  width: 100%;\n  height: 500px;\n  display: flex;\n  margin-bottom: 10px;\n"])));
 exports.SkeletonContent = SkeletonContent;
-var SkeletonInformation = _styledComponents.default.div(_templateObject6 || (_templateObject6 = _taggedTemplateLiteral(["\n  width: 70%;\n  height: 100%;\n  display: flex;\n  flex-direction: column;\n  justify-content: space-between;\n  margin-right: 10px;\n  ", "  \n"])), function (props) {
+var SkeletonInformation = _styledComponents.default.div(_templateObject6 || (_templateObject6 = _taggedTemplateLiteral(["\n  width: 70%;\n  height: 100%;\n  display: flex;\n  flex-direction: column;\n  justify-content: space-between;\n  margin-right: 10px;\n  ", "\n"])), function (props) {
   var _props$theme;
   return ((_props$theme = props.theme) === null || _props$theme === void 0 ? void 0 : _props$theme.rtl) && (0, _styledComponents.css)(_templateObject7 || (_templateObject7 = _taggedTemplateLiteral(["\n    margin-left: 10px;\n    margin-right: 0;\n  "])));
 });
 exports.SkeletonInformation = SkeletonInformation;
-var SkeletonSide = _styledComponents.default.div(_templateObject8 || (_templateObject8 = _taggedTemplateLiteral(["\n  width: 30%;\n  height: 100%;\n  margin-left: 10px;\n  ", "  \n"])), function (props) {
+var SkeletonSide = _styledComponents.default.div(_templateObject8 || (_templateObject8 = _taggedTemplateLiteral(["\n  width: 30%;\n  height: 100%;\n  margin-left: 10px;\n  ", "\n"])), function (props) {
   var _props$theme2;
-  return ((_props$theme2 = props.theme) === null || _props$theme2 === void 0 ? void 0 : _props$theme2.rtl) && (0, _styledComponents.css)(_templateObject9 || (_templateObject9 = _taggedTemplateLiteral(["\n    margin-right: 10px;\n    margin-left: 0;    \n  "])));
+  return ((_props$theme2 = props.theme) === null || _props$theme2 === void 0 ? void 0 : _props$theme2.rtl) && (0, _styledComponents.css)(_templateObject9 || (_templateObject9 = _taggedTemplateLiteral(["\n    margin-right: 10px;\n    margin-left: 0;\n  "])));
 });
 exports.SkeletonSide = SkeletonSide;
--- _modules/components/PagesList/styles.js
+++ _modules/components/PagesList/styles.js
@@ -8,5 +8,17 @@ var _styledComponents = _interopRequireDefault(require("styled-components"));
 var _templateObject;
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
-var PagesListContainer = _styledComponents.default.div(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n  display: inline-flex;\n  flex-direction: column;\n  width: 100%;\n  padding: 0 30px 10px 30px;\n  a{\n    display: inline;\n    text-decoration: none;\n    color: #000\n  }\n"])));
+var PagesListContainer = _styledComponents.default.div(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n  display: inline-flex;\n  flex-direction: column;\n  width: 100%;\n  padding: 0 30px 10px 30px;\n  color: ", " !important;\n\n  > span * {\n    font-family: ", " !important;\n    color: ", " !important;\n  }\n  a {\n    display: inline;\n    text-decoration: none;\n    color: #000\n  }\n"])), function (_ref) {
+  var _theme$colors$tertiar, _theme$colors;
+  var theme = _ref.theme;
+  return (_theme$colors$tertiar = theme === null || theme === void 0 ? void 0 : (_theme$colors = theme.colors) === null || _theme$colors === void 0 ? void 0 : _theme$colors.tertiary) !== null && _theme$colors$tertiar !== void 0 ? _theme$colors$tertiar : '#FFF';
+}, function (_ref2) {
+  var _theme$fonts, _theme$fonts$special;
+  var theme = _ref2.theme;
+  return theme === null || theme === void 0 ? void 0 : (_theme$fonts = theme.fonts) === null || _theme$fonts === void 0 ? void 0 : (_theme$fonts$special = _theme$fonts.special) === null || _theme$fonts$special === void 0 ? void 0 : _theme$fonts$special.name;
+}, function (_ref3) {
+  var _theme$colors$tertiar2, _theme$colors2;
+  var theme = _ref3.theme;
+  return (_theme$colors$tertiar2 = theme === null || theme === void 0 ? void 0 : (_theme$colors2 = theme.colors) === null || _theme$colors2 === void 0 ? void 0 : _theme$colors2.tertiary) !== null && _theme$colors$tertiar2 !== void 0 ? _theme$colors$tertiar2 : '#FFF';
+});
 exports.PagesListContainer = PagesListContainer;
