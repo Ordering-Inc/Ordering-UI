b7e5d65ebc1afafa1d344792e75878a76acec2fe
Carlos Quiroz <carlos.q@ordering.co>
2024-03-20 12:21:34 -0400

    [FEAT] update business map component

--- src/components/BusinessesMap/index.js
+++ src/components/BusinessesMap/index.js
@@ -13,7 +13,7 @@ const BusinessesMapUI = (props) => {
   } = props
 
   const [configState] = useConfig()
-  const [orderState] = useOrder()
+  const [{ options, loading }] = useOrder()
   const theme = useTheme()
   const googleMapsControls = {
     defaultZoom: 18,
@@ -29,43 +29,29 @@ const BusinessesMapUI = (props) => {
   }
 
   return (
-    <>
-      {props.beforeElements?.map((BeforeElement, i) => (
-        <React.Fragment key={i}>
-          {BeforeElement}
-        </React.Fragment>))}
-      {props.beforeComponents?.map((BeforeComponent, i) => (
-        <BeforeComponent key={i} {...props} />))}
-      <WrapperMap disabled={orderState.loading}>
-        <GoogleMapsMap
-          useMapWithBusinessZones
-          deactiveAlerts
-          businessMap
-          apiKey={configState?.configs?.google_maps_api_key?.value}
-          location={userLocation}
-          locations={businessLocations}
-          mapControls={googleMapsControls}
-          maxLimitLocation={parseInt(configState?.configs?.meters_to_change_address?.value)}
-          onBusinessClick={onBusinessClick}
-          businessZones={businessList?.map(business => business?.zones)}
-          fallbackIcon={theme.images?.dummies?.businessLogo}
-          setErrors={setErrors}
-          fixedLocation={!userLocation && (businessLocations?.length
-            ? { ...businessLocations[0], hideicon: true }
-            : {
-              lat: parseFloat(configState?.configs?.location_default_latitude?.value ?? 0, 10),
-              lng: parseFloat(configState?.configs?.location_default_longitude?.value ?? 0, 10)
-            }
-          )}
-        />
-      </WrapperMap>
-      {props.afterComponents?.map((AfterComponent, i) => (
-        <AfterComponent key={i} {...props} />))}
-      {props.afterElements?.map((AfterElement, i) => (
-        <React.Fragment key={i}>
-          {AfterElement}
-        </React.Fragment>))}
-    </>
+    <WrapperMap disabled={loading}>
+      <GoogleMapsMap
+        useMapWithBusinessZones
+        deactiveAlerts
+        businessMap
+        apiKey={configState?.configs?.google_maps_api_key?.value}
+        location={userLocation}
+        locations={businessLocations}
+        mapControls={googleMapsControls}
+        maxLimitLocation={parseInt(configState?.configs?.meters_to_change_address?.value)}
+        onBusinessClick={options.type !== 1 ? onBusinessClick : () => {}}
+        businessZones={businessList?.map(business => business?.zones)}
+        fallbackIcon={theme.images?.dummies?.businessLogo}
+        setErrors={setErrors}
+        fixedLocation={!userLocation && (businessLocations?.length
+          ? { ...businessLocations[0], hideicon: true }
+          : {
+            lat: parseFloat(configState?.configs?.location_default_latitude?.value ?? 0, 10),
+            lng: parseFloat(configState?.configs?.location_default_longitude?.value ?? 0, 10)
+          }
+        )}
+      />
+    </WrapperMap>
   )
 }
 
