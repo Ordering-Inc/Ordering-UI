d53cd049a5b4b3995b3ca2d4525456aee2b78605
YisusJe <jesus@ordering.co>
2024-04-02 18:07:27 -0400

    short code

--- src/themes/callcenterOriginal/src/components/InputPhoneNumber/index.js
+++ src/themes/callcenterOriginal/src/components/InputPhoneNumber/index.js
@@ -47,7 +47,9 @@ export const InputPhoneNumber = (props) => {
     }
     const numberParser = parsePhoneNumber(number)
     const validations = ['0', '+']
-    if ((phoneRef?.current?.value?.startsWith('16') || validations.includes(phoneRef?.current?.value[0])) && UKCodes.includes(numberParser?.countryCallingCode)) {
+    const startsWithValidations = ['16']
+    const hasStartValidation = startsWithValidations.some(validation => phoneRef?.current?.value?.startsWith?.(validation))
+    if ((hasStartValidation || validations.includes(phoneRef?.current?.value[0])) && UKCodes.includes(numberParser?.countryCallingCode)) {
       const numberInput = phoneRef?.current?.value.replace('-', '')
       let numberRaw = ''
       numberInput?.split(' ')?.filter((_splited, i) => i > 0 || (i === 0 && (_splited[0] === '0' || _splited.startsWith('16'))))?.map(splited => {
