01a805423acd5c58fe74264783c18cfbe83e69aa
Jesus David <50583541+YisusJe@users.noreply.github.com>
2024-05-14 06:49:54 -0400

    [FIX] driver tips required for google pay (#2016)
    Co-authored-by: YisusJe <jesus@ordering.co>

--- src/themes/five/src/components/Checkout/index.js
+++ src/themes/five/src/components/Checkout/index.js
@@ -588,6 +588,7 @@ const CheckoutUI = (props) => {
                 openUserModal={setIsOpen}
                 paymethodClicked={paymethodClicked}
                 setPaymethodClicked={setPaymethodClicked}
+                validateDriverTipField={validateDriverTipField}
               />
             </PaymentMethodContainer>
           )}
